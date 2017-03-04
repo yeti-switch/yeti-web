734d6f0d40d39f2c128e00a42ca3ff738992f7b6
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2017-03-05 00:18:06 +0200

    Vendor validations for Dialpeer, Gateway and Gateway Group. fixes #1

--- app/models/gateway_group.rb
+++ app/models/gateway_group.rb
@@ -10,17 +10,18 @@
 
 class GatewayGroup < ActiveRecord::Base
   
-  belongs_to :vendor, -> { where vendor: true }, class_name: 'Contractor'
+  belongs_to :vendor, class_name: Contractor
   has_many :gateways, dependent: :restrict_with_error
   has_many :dialpeers, dependent: :restrict_with_error
 
   has_paper_trail class_name: 'AuditLogItem'
 
-
-
   validates_presence_of :name
   validates_uniqueness_of :name, allow_blank: false
   validates_presence_of :vendor
+
+  validate :contractor_is_vendor
+  validate :vendor_can_be_changed
 
   def display_name
     "#{self.name} | #{self.id}"
@@ -28,6 +29,19 @@ class GatewayGroup < ActiveRecord::Base
 
   def have_valid_gateways?
     gateways.where("enabled and allow_termination").count>0
+  end
+
+  protected
+
+  def contractor_is_vendor
+     self.errors.add(:vendor, "Is not vendor") unless self.vendor && self.vendor.vendor
+  end
+
+  def vendor_can_be_changed
+    if vendor_id_changed?
+      self.errors.add(:vendor, "can't be changed because Gateway Group contain gateways") if gateways.any?
+      self.errors.add(:vendor, "can't be changed because Gateway Group belongs to dialpeers") if dialpeers.any?
+    end
   end
 
 end
