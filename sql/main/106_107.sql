14319af3151856a865e630300b910d8b4ac51016
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2017-04-05 00:23:32 +0300

    Version bump and migration fix

--- sql/main/106_107.sql
+++ sql/main/106_107.sql
@@ -1,5 +1,5 @@
 begin;
-insert into sys.version(number,comment) values(104,'Dialpeer next rate was changed');
+insert into sys.version(number,comment) values(107,'Dialpeer next rate was changed');
 
 ALTER TABLE class4.dialpeer_next_rates RENAME COLUMN rate TO next_rate;
 ALTER TABLE class4.dialpeer_next_rates ADD COLUMN initial_rate numeric;
