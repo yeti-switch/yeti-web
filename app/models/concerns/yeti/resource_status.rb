0e840e5a7149b7d8f9d8b2d8df54203ce34a59bb
Igor Fedoronchuk <fedoronchuk@gmail.com>
2017-07-01 15:16:21 +0300

    Revert "Revert "move modules and classes from lib to models that requires autoload""
    This reverts commit f348bca361cf960ab742c4f3597c86fb1e4b2770.

---
+++ app/models/concerns/yeti/resource_status.rb
@@ -0,0 +1,28 @@
+module Yeti
+  module ResourceStatus
+    extend ActiveSupport::Concern
+    included do
+      scope :disabled, -> { base.where enabled: false }
+      scope :enabled, -> { base.where enabled: true }
+
+      def status_sym
+        self.enabled? ? :enabled : :disabled
+      end
+
+      def enable!
+        self.enabled = true
+        save!
+      end
+
+      def disable!
+        self.enabled = false
+        save!
+      end
+
+      def disabled?
+        !self.enabled
+      end
+
+    end
+  end
+end
