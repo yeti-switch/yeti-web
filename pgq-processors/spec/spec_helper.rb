c4665e3e668fb58c7a4ca9fe98a13a0a32d3fbd5
max <maxim21214@gmail.com>
2017-07-06 16:18:53 +0300

    rebase

--- pgq-processors/spec/spec_helper.rb
+++ pgq-processors/spec/spec_helper.rb
@@ -1,6 +1,14 @@
-require 'webmock/rspec'
+require 'active_record'
+require 'http_logger'
+require 'pgq'
+require 'active_resource'
+require 'active_support/core_ext/string'
+require 'active_resource/persistent'
+require 'syslog-logger'
+require File.join(File.dirname(__FILE__), '../lib/json_coder')
+require File.join(File.dirname(__FILE__), '../lib/shutdown')
 
-require File.join(File.dirname(__FILE__), '../pgq_env')
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
