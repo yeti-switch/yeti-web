35054f8451b8a2444de156bd9718c6d9dca5cec1
Unnumbered <andrewamigos@gmail.com>
2017-07-13 00:50:47 +0700

    gateway api docs

---
+++ spec/factories/codec_group.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :codec_group do
+    sequence(:name) { |n| "codec_group#{n}" }
+    codec_group_codecs do
+      [
+        build(:codec_group_codec, codec_id: 6, priority: 1),
+        build(:codec_group_codec, codec_id: 7, priority: 2)
+      ]
+    end
+  end
+end
---
+++ spec/factories/codec_group_codec.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :codec_group_codec do
+    codec_id 6
+  end
+end
