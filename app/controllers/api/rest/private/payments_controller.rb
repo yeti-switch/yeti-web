baac59d4a733346572fd8b55a8f1f38bfc38ecae
Unnumbered <andrewamigos@gmail.com>
2017-07-13 18:29:41 +0700

    payments api doc

---
+++ app/controllers/api/rest/private/payments_controller.rb
@@ -0,0 +1,34 @@
+class Api::Rest::Private::PaymentsController < Api::RestController
+
+  after_action only: [:index] do
+    send_x_headers(@payments)
+  end
+
+  before_action only: [:show, :update, :destroy] do
+    @payment = Payment.find(params[:id])
+  end
+
+  def index
+    @payments = resource_collection(Payment.all)
+    respond_with @payments
+  end
+
+  def show
+    respond_with(@payment, location: nil)
+  end
+
+  def create
+    @payment = Payment.create(payment_params)
+    respond_with(@payment, location: nil)
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(
+      :account_id,
+      :amount,
+      :notes
+    )
+  end
+end
