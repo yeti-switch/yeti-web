dd6f7b64016004b8690fa8ca041cbb3092fb1a00
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2017-09-05 00:14:07 +0300

    Some deprecation warnings was fixed. closes #84 (#96)
    * Some deprecation warnings was fixed. closes #84

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,8 @@ Yeti::Application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.serve_static_files = true
+
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
