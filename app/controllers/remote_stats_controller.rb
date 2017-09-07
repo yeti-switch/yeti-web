ef9ff96b31f2607324c6bbddb8918ba96d019aca
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2017-09-07 23:30:08 +0300

    fix remote_stats_controller after rails upgrade

--- app/controllers/remote_stats_controller.rb
+++ app/controllers/remote_stats_controller.rb
@@ -1,47 +1,51 @@
 class RemoteStatsController < ApplicationController
 
+  #ActiveModel::Serializers::JSON.include_root_in_json disabled because out JS charts use old format - without root
+  #TODO fix JS and enable include_root_in_json
+
   before_action :authenticate_admin_user!
+
   respond_to :json
 
   def profit
     expires_in 10.minutes, public: true
-    respond_with(Stats::TrafficCustomerAccount.stats(:profit, 48))
+    render json: Stats::TrafficCustomerAccount.stats(:profit, 48).to_json(root: false)
   end
 
   def duration
     expires_in 10.minutes, public: true
-    respond_with(Stats::TrafficCustomerAccount.stats(:duration, 48))
+    render json:Stats::TrafficCustomerAccount.stats(:duration, 48).to_json(root: false)
   end
 
   def nodes
     expires_in 1.minutes, public: true
-    respond_with(Stats::ActiveCall.to_stacked_chart)
+    render json:Stats::ActiveCall.to_stacked_chart.to_json(root: false)
   end
 
   def vendors_traffic
     expires_in 2.minutes, public: true
-    respond_with(Stats::TrafficVendorAccount.to_chart(params[:id]))
+    render json:Stats::TrafficVendorAccount.to_chart(params[:id]).to_json(root: false)
   end
 
   def customers_traffic
     expires_in 2.minutes, public: true
-    respond_with(Stats::TrafficCustomerAccount.to_chart(params[:id]))
+    render json:Stats::TrafficCustomerAccount.to_chart(params[:id]).to_json(root: false)
   end
 
 
   def hour_nodes
     #expires_in 1.minutes, public: true
-    respond_with(Stats::ActiveCall.to_stacked_chart(1))
+    render json:Stats::ActiveCall.to_stacked_chart(1).to_json(root: false)
   end
 
   def node
     expires_in 1.minutes, public: true
-    respond_with(Stats::ActiveCall.to_chart(params[:id]))
+    render json:Stats::ActiveCall.to_chart(params[:id]).to_json(root: false)
   end
 
   def aggregated_node
     expires_in 30.minutes, public: true
-    respond_with(Stats::AggActiveCall.to_chart(params[:id]))
+    render json:Stats::AggActiveCall.to_chart(params[:id]).to_json(root: false)
   end
 
   def cdrs_summary
@@ -83,60 +87,60 @@ class RemoteStatsController < ApplicationController
 
   def term_gateway
     expires_in 2.minutes, public: true
-    respond_with(Stats::ActiveCallTermGateway.to_chart(params[:id]))
+    render json:Stats::ActiveCallTermGateway.to_chart(params[:id]).to_json(root: false)
   end
 
   def aggregated_term_gateway
     expires_in 10.minutes, public: true
-    respond_with(Stats::AggActiveCallTermGateway.to_chart(params[:id]))
+    render json:Stats::AggActiveCallTermGateway.to_chart(params[:id]).to_json(root: false)
   end
 
   def orig_gateway
     expires_in 2.minutes, public: true
-    respond_with(Stats::ActiveCallOrigGateway.to_chart(params[:id]))
+    render json: Stats::ActiveCallOrigGateway.to_chart(params[:id]).to_json(root: false)
   end
 
 
   def aggregated_orig_gateway
     expires_in 10.minutes, public: true
-    respond_with(Stats::AggActiveCallOrigGateway.to_chart(params[:id]))
+    render json:Stats::AggActiveCallOrigGateway.to_chart(params[:id]).to_json(root: false)
   end
 
   def aggregated_customer_account
     expires_in 10.minutes, public: true
-    respond_with(Stats::AggActiveCallCustomerAccount.to_chart(params[:id]))
+    render json:Stats::AggActiveCallCustomerAccount.to_chart(params[:id]).to_json(root: false)
   end
 
   def aggregated_vendor_account
     expires_in 10.minutes, public: true
-    respond_with(Stats::AggActiveCallVendorAccount.to_chart(params[:id]))
+    render json:Stats::AggActiveCallVendorAccount.to_chart(params[:id]).to_json(root: false)
   end
 
   ######
 
   def account_active_calls
     expires_in 2.minutes, public: true
-    respond_with(Stats::ActiveCallAccount.new(params[:id]).to_chart)
+    render json:Stats::ActiveCallAccount.new(params[:id]).to_chart.to_json(root: false)
   end
 
   def customer_account
     expires_in 2.minutes, public: true
-    respond_with(Stats::ActiveCallCustomerAccount.to_chart(params[:id]))
+    render json: Stats::ActiveCallCustomerAccount.to_chart(params[:id]).to_json(root: false)
   end
 
   def vendor_account
     expires_in 2.minutes, public: true
-    respond_with(Stats::ActiveCallVendorAccount.to_chart(params[:id]))
+    render json:Stats::ActiveCallVendorAccount.to_chart(params[:id]).to_json(root: false)
   end
 
   def gateway_pdd_distribution
     expires_in 2.minutes, public: true
-    respond_with(Stats::TerminationQualityStat.to_pdd_gateway_chart(params[:id]))
+    render json:Stats::TerminationQualityStat.to_pdd_gateway_chart(params[:id]).to_json(root: false)
   end
 
   def dialpeer_pdd_distribution
     expires_in 2.minutes, public: true
-    respond_with(Stats::TerminationQualityStat.to_pdd_dialpeer_chart(params[:id]))
+    render json:Stats::TerminationQualityStat.to_pdd_dialpeer_chart(params[:id]).to_json(root: false)
   end
 
   private
