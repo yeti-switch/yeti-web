52f4a3c94498feaf16b4a50b95cf5f5adae7e6dc
Gena <workgena@gmail.com>
2017-09-23 20:46:40 +0300

    API Customer: CDR resource

---
+++ app/resources/api/rest/customer/v1/cdr_resource.rb
@@ -0,0 +1,64 @@
+class Api::Rest::Customer::V1::CdrResource < Api::Rest::Customer::V1::BaseResource
+  model_name 'Cdr::Cdr'
+
+  key_type :uuid
+  primary_key :uuid
+
+  attributes :time_start,
+             :time_connect,
+             :time_end,
+             :duration,
+             :success,
+             :destination_initial_interval,
+             :destination_initial_rate,
+             :destination_next_interval,
+             :destination_next_rate,
+             :destination_fee,
+             :customer_price,
+             :src_name_in,
+             :src_prefix_in,
+             :from_domain,
+             :dst_prefix_in,
+             :to_domain,
+             :ruri_domain,
+             :diversion_in,
+             :local_tag,
+             :lega_disconnect_code,
+             :lega_disconnect_reason,
+             :lega_rx_payloads,
+             :lega_tx_payloads,
+             :auth_orig_transport_protocol_id,
+             :auth_orig_ip,
+             :auth_orig_port,
+             :lega_rx_bytes,
+             :lega_tx_bytes,
+             :lega_rx_decode_errs,
+             :lega_rx_no_buf_errs,
+             :lega_rx_parse_errs,
+             :src_prefix_routing,
+             :dst_prefix_routing,
+             :destination_prefix
+
+  has_one :auth_orig_transport_protocol, class_name: 'TransportProtocol'
+
+  filter :is_last_cdr, default: true
+
+  filters :success, :src_prefix_routing, :dst_prefix_routing, :duration,
+          :lega_disconnect_code, :lega_disconnect_reason, :src_prefix_in,
+          :dst_prefix_in, :diversion_in, :src_name_in, :local_tag
+
+  ransack_filter :time_start_gteq
+  ransack_filter :time_start_lteq
+
+  # TODO: move to BaseResource
+  def self.records(options = {})
+    apply_allowed_accounts(super(options), options)
+  end
+
+  def self.apply_allowed_accounts(_records, options)
+    context = options[:context]
+    scope = _records.where_customer(context[:customer_id])
+    scope = scope.where_account(context[:allowed_account_ids]) if context[:allowed_account_ids].present?
+    scope
+  end
+end
---
+++ spec/support/examples/jsonapi_request_with_filter.rb
@@ -0,0 +1,22 @@
+RSpec.shared_examples :jsonapi_request_with_filter do
+
+  let(:attr_value) { 'Default String value to make testing easier' }
+  let(:request_filters) { { attr_name => attr_value.to_s } }
+
+  before do
+    @expected_record = expected_record
+  end
+
+  subject do
+    get :index, filter: request_filters
+  end
+
+  it 'response contains only one fitting record' do
+    subject
+    expect(response_data).to match_array(
+      [
+        hash_including('id' => @expected_record.uuid)
+      ]
+    )
+  end
+end
