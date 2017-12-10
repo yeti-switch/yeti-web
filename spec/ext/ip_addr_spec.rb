7f91201f0606223b8c15d4edf3a30d87ef991a8b
Gena <workgena@gmail.com>
2017-12-10 19:03:08 +0200

    tests on System::IPAccess-controller and IPAddr#cidr_mask method

---
+++ spec/ext/ip_addr_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper'
+
+describe IPAddr do
+
+  describe '#cidr_mask' do
+    
+    subject do
+      described_class.new(ip).cidr_mask
+    end
+
+    context 'with valid IPv4' do
+      let(:ip) { '192.168.0.0/16' }
+
+      it { is_expected.to eq(16) }
+    end
+
+    context 'with valid IPv6' do
+      let(:ip) { '2001:67c:1324:111::1/64' }
+
+      it { is_expected.to eq(64) }
+    end
+    
+    context 'with invalid IPv4' do
+      let(:ip) { '1.1.1.1/34' }
+      
+      it { expect { subject }.to raise_error(IPAddr::InvalidPrefixError) }
+    end
+
+    context 'with invalid IPv6' do
+      let(:ip) { '2001:67c:1324:111::1/129' }
+
+      it { expect { subject }.to raise_error(IPAddr::InvalidPrefixError) }
+    end
+  end
+
+end
+
