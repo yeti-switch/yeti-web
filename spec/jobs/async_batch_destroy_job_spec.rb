ebbce9de9c75d3be189d18caa641d5b62371c7f0
azamyatin <stekbox@gmail.com>
2018-01-22 18:33:50 +0700

    tests fixed

--- spec/jobs/async_batch_destroy_job_spec.rb
+++ spec/jobs/async_batch_destroy_job_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe AsyncBatchDestroyJob, type: :job do
     include_context :init_destination, id: 2, initial_rate: 0.5
     include_context :init_destination, id: 3, initial_rate: 0.7
 
-    subject { described_class.new(model_class, sql_query).perform}
+    subject { described_class.new(model_class, sql_query, who_is).perform }
 
     before :each do
       stub_const('AsyncBatchDestroyJob::BATCH_SIZE', 2)
     end
+
+    let(:who_is) { create :admin_user }
 
     context 'incorrect class_name' do
       let(:model_class) { 'Fake' }
--- spec/jobs/async_batch_update_job_spec.rb
+++ spec/jobs/async_batch_update_job_spec.rb
@@ -7,11 +7,13 @@ RSpec.describe AsyncBatchUpdateJob, type: :job do
     include_context :init_destination, id: 2, initial_rate: 0.5
     include_context :init_destination, id: 3, initial_rate: 0.7
 
-    subject { described_class.new(model_class, sql_query, changes).perform}
+    subject { described_class.new(model_class, sql_query, changes, who_is).perform }
 
     before :each do
       stub_const('AsyncBatchUpdateJob::BATCH_SIZE', 2)
     end
+
+    let(:who_is) { create :admin_user }
 
     context 'incorrect class_name' do
       let(:model_class) { 'Fake' }
