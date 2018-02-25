4796bb8e4249d23ca0550681fedad5cbc51968ec
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2018-02-25 23:25:30 +0200

    switch15 schema

--- db/migrate/20180216154625_add_external_id_to_customer_auth.rb
+++ db/migrate/20180216154625_add_external_id_to_customer_auth.rb
@@ -1,6 +1,18 @@
 class AddExternalIdToCustomerAuth < ActiveRecord::Migration
-  def change
-    add_column 'class4.customers_auth', :external_id, :bigint
-    add_index  'class4.customers_auth', :external_id, unique: true, name: 'customers_auth_external_id_idx'
+  def up
+    execute %q{
+      alter table class4.customers_auth add external_id bigint;
+      alter table class4.customers_auth_normalized add external_id bigint;
+      create unique index  customers_auth_external_id_idx on class4.customers_auth using btree(external_id);
+    }
+  end
+
+  def down
+    execute %q{
+      alter table class4.customers_auth drop column external_id;
+      alter table class4.customers_auth_normalized drop column external_id;
+      --drop index class4.customers_auth_external_id_idx;
+  }
+
   end
 end
