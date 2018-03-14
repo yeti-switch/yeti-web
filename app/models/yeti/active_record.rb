3b817aed66b4acd20af34f20789d0fda7808d25f
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2018-03-14 23:20:58 +0200

    move to switch15 schema

--- app/models/yeti/active_record.rb
+++ app/models/yeti/active_record.rb
@@ -69,7 +69,7 @@ class Yeti::ActiveRecord < ActiveRecord::Base
   end
 
   DB_VER = LazyObject.new { db_version }
-  ROUTING_SCHEMA="switch14"
+  ROUTING_SCHEMA="switch15"
 
   PG_MAX_INT = 2147483647
   PG_MIN_INT = 2147483647
