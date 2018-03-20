68f351e37fc9f4f38d65a17ca0ca4ae6ece58a94
Igor Gonchar <gigorok@gmail.com>
2018-03-20 18:57:44 +0200

    admin API CDRs exports endpoint

---
+++ spec/controllers/api/rest/admin/cdr/cdr_exports_controller_spec.rb
@@ -0,0 +1,112 @@
+require 'spec_helper'
+
+describe Api::Rest::Admin::Cdr::CdrExportsController, type: :controller do
+
+  let(:admin_user) { create :admin_user }
+  let(:auth_token) { ::Knock::AuthToken.new(payload: { sub: admin_user.id }).token }
+
+  before do
+    request.accept = 'application/vnd.api+json'
+    request.headers['Content-Type'] = 'application/vnd.api+json'
+    request.headers['Authorization'] = auth_token
+  end
+
+  describe 'POST create' do
+    subject { post :create, payload }
+    let(:payload) do
+      {
+        data: {
+          type: 'cdr-exports',
+          attributes: {
+            fields: fields,
+            filters: filters
+          }
+        }
+      }
+    end
+    let(:fields) do
+      [
+        'success'
+      ]
+    end
+    let(:filters) do
+      {
+        'time_start_gteq' => '2018-01-01',
+        'time_start_lteq' => '2018-03-01'
+      }
+    end
+
+    it 'http status should eq 201' do
+      subject
+      expect(response.status).to eq(201)
+    end
+
+    it 'CDR export should be created' do
+      expect { subject }.to change { CdrExport.count }.by(1)
+    end
+
+    it 'created CDR export has valid attributes' do
+      subject
+      expect(CdrExport.last!).to have_attributes(
+        status: CdrExport::STATUS_PENDING,
+        fields: fields,
+        filters: filters
+      )
+    end
+
+    it 'response body should be valid' do
+      subject
+      cdr_export = CdrExport.last!
+      expect(JSON.parse(response.body)['data']).to match(
+        hash_including(
+          'id' => cdr_export.id.to_s,
+          'type' => 'cdr-exports',
+          'attributes' => {
+            'export-type' => 'Base',
+            'callback-url' => nil,
+            'status' => CdrExport::STATUS_PENDING,
+            'fields' => fields,
+            'filters' => filters,
+            'created-at' => cdr_export.created_at.iso8601(3)
+          }
+        )
+      )
+    end
+
+    it 'delayed job should be created' do
+      expect { subject }.to have_enqueued_job(Worker::CdrExportJob)
+    end
+
+    context 'with non supporting filters' do
+      let(:filters) do
+        super().merge('unknown-filter' => '123')
+      end
+      it 'validation error should be present' do
+        subject
+        expect(response.status).to eq(422)
+        expect(JSON.parse(response.body)['errors']).to match_array(
+          hash_including(
+            'detail' => 'filters - unknown_filter not allowed'
+          )
+        )
+      end
+    end
+
+    context 'with unknown fields' do
+      let(:fields) do
+        ['unknown_field']
+      end
+      it 'validation error should be present' do
+        subject
+        expect(response.status).to eq(422)
+        expect(JSON.parse(response.body)['errors']).to match_array(
+          hash_including(
+            'detail' => 'fields - unknown_field not allowed'
+          )
+        )
+      end
+    end
+
+  end
+
+end
