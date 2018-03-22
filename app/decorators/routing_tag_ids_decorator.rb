cda649c5de1972dfdaba0e46b09839736c208094
Gena <workgena@gmail.com>
2018-03-22 16:58:48 +0200

    ActiveAdmin CDRs display routing_tag names
    ActiveAdmin "CDRs" and "CDR Archive"
    display "Routing Tags"-names instead if IDs

--- app/decorators/routing_tag_ids_decorator.rb
+++ app/decorators/routing_tag_ids_decorator.rb
@@ -3,7 +3,7 @@ module RoutingTagIdsDecorator
   # TODO: this is very very bad for index page
   # replace with has_many :routing_tags
   def routing_tags
-    if model.routing_tag_ids.empty?
+    unless model.routing_tag_ids.present?
       return h.content_tag(:span, 'NOT TAGGED', class: 'status_tag')
     end
     model.routing_tag_ids.map do |id|
