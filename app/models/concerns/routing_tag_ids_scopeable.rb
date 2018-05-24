e69d3d75429fddddc5c012ae3d100f7f44b093ea
Igor Gonchar <gigorok@gmail.com>
2018-05-25 00:12:23 +0300

    fix filtering routing tag detection rules (#317)

--- app/models/concerns/routing_tag_ids_scopeable.rb
+++ app/models/concerns/routing_tag_ids_scopeable.rb
@@ -7,8 +7,8 @@ module RoutingTagIdsScopeable
       where("yeti_ext.tag_compare(routing_tag_ids, ARRAY[#{id.join(',')}], routing_tag_mode_id)>0")
     end
 
-    scope :tagged, ->(value) do
-      if ActiveRecord::Type::Boolean.new.type_cast_from_user(value)
+    scope :tagged, ->(*value) do
+      if ActiveModel::Type::Boolean.new.cast(value)
         where("routing_tag_ids <> '{}'") # has tags
       else
         where("routing_tag_ids = '{}'") # no tags
