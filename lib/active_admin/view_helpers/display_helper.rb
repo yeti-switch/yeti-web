20f1c57b7d271e099a0160a466e88df7fd9e201f
Denis Talakevich <senid231@gmail.com>
2018-06-23 15:47:16 +0300

    speed up gateways page by using cached status_tag for boolean value and faster check if value is boolean

--- lib/active_admin/view_helpers/display_helper.rb
+++ lib/active_admin/view_helpers/display_helper.rb
@@ -1,6 +1,11 @@
 module ActiveAdmin
   module ViewHelpers
     module DisplayHelper
+      STATUS_TAG_CACHE = [
+          '<span class="status_tag yes">Yes</span>'.html_safe.freeze,
+          '<span class="status_tag no">No</span>'.html_safe.freeze
+      ].freeze
+      BOOLEAN_VALUES = [true, false].freeze
 
       alias_method :original_pretty_format, :pretty_format
 
@@ -11,6 +16,15 @@ module ActiveAdmin
         end
 
         original_pretty_format(object)
+      end
+
+      def boolean_status_tag(value)
+        STATUS_TAG_CACHE[value ? 0 : 1]
+      end
+
+      def is_boolean_val?(value)
+        BOOLEAN_VALUES.include?(value)
+        # value.is_a?(TrueClass) || value.is_a?(FalseClass)
       end
 
     end
