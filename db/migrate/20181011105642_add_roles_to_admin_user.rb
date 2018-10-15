1a05fd080e9a21e3da9e76bbc90f11e39316f11a
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2018-10-16 00:42:51 +0300

    structure upgrade

--- db/migrate/20181011105642_add_roles_to_admin_user.rb
+++ db/migrate/20181011105642_add_roles_to_admin_user.rb
@@ -2,6 +2,7 @@ class AddRolesToAdminUser < ActiveRecord::Migration[5.1]
   def up
     execute %q{
       alter table gui.admin_users add  roles varchar[] not null default array['root']::varchar[];
+      alter table gui.admin_users alter column roles drop default;
       alter table gui.admin_users drop column "group";
     }
   end
