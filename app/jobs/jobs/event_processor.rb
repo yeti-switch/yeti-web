5e448c84a66215869991915289cbb5daa9c493a0
Denis Talakevich <senid231@gmail.com>
2018-10-19 17:56:59 +0300

    fix event processor when api returns error #386

--- app/jobs/jobs/event_processor.rb
+++ app/jobs/jobs/event_processor.rb
@@ -3,34 +3,40 @@ module Jobs
 
 
     def execute
-
-      events.each do |ev|
-        begin
-          Event.transaction do
-            process_event(ev)
-            ev.destroy!
-          end
-        rescue StandardError => e
-          logger.warn e.message
-          logger.warn e.backtrace.join("\n")
-          ev.retries+=1
-          ev.last_error = e.message
-          ev.save!
+      processed_count = 0
+      events_count = events.count
+      events.find_each do |ev|
+        Event.transaction do
+          processed_count += 1 if safe_process_event(ev)
         end
       end
-      events.count
-    end
-
-    protected
-
-    def events
-      @events ||= Event.eager_load(:node).order('created_at')
+      logger.info "Job #{type} processed #{processed_count}/#{events_count} events."
     end
 
     def process_event(ev)
       args = ev.command.split /\s+/
       logger.info "RPC send #{args} to node #{ev.node.id}"
       ev.node.api.rpc_send(args.shift, args)
+    end
+
+    private
+
+    def events
+      @events ||= Event.preload(:node).order('created_at')
+    end
+
+    def safe_process_event(ev)
+      process_event(ev)
+      ev.destroy!
+      true
+    rescue StandardError => e
+      logger.warn { "Processing event ##{ev.id} was failed" }
+      logger.warn { "<#{e.class}>: #{e.message}" }
+      logger.warn { e.backtrace.join("\n") }
+      ev.retries += 1
+      ev.last_error = e.message
+      ev.save!
+      false
     end
 
   end
