f5a5edaa26670c2c318e37cac26aa4da8c7782eb
Gena M <workgena@gmail.com>
2018-11-26 15:51:01 +0200

    Fix "Create new Account" feature spec
    Error:
    The feature spec "Create new Account" was working with Poltergeist,
    and is broken with Chrome-headless.
    
    Background:
    Chrome-webdriver fills in the form "New Account", and when fills in the
    "Send balance notifications to" it somehow do not "focus out"(blur) out
    of the input. And the selecting of the "Timezone" fails because the
    click on "chosen" does the "focus out " from "Send balance notifications" and
    not opens "Timezone"-chosen.
    
    Solution:
    After selecting the "Send balance notifications" we do one dummy click
    on `<label>Timezone</label>` to emulate "focus out".
    So, the next click on Timezone-chosen works correctly.

--- spec/features/billing/accounts/new_account_spec.rb
+++ spec/features/billing/accounts/new_account_spec.rb
@@ -6,50 +6,46 @@ describe 'Create new Account', type: :feature, js: true do
   before do
     @customer = create(:customer)
     @tz = create(:timezone)
+
     visit new_account_path
   end
 
   include_context :fill_form, 'new_account' do
-
-
     let(:attributes) do
       {
-          name: "Account",
-          contractor_id: -> {
-            chosen_pick('#account_contractor_id+div', text: @customer.name)
-          },
-          min_balance: -100,
-          max_balance: 100,
-          balance_low_threshold: -90,
-          balance_high_threshold: 90,
-          origination_capacity: 100,
-          termination_capacity: 50,
-          timezone_id: -> {
-            chosen_pick('#account_timezone_id+div', text: @tz.display_name)
-          }
+        name: 'Account',
+        contractor_id: -> {
+          chosen_pick('#account_contractor_id+div', text: @customer.name)
+        },
+        min_balance: -100,
+        max_balance: 100,
+        balance_low_threshold: -90,
+        balance_high_threshold: 90,
+        origination_capacity: 100,
+        termination_capacity: 50,
+        timezone_id: -> {
+          find('label[for="account_timezone_id"]').click # focus-out from "Send balance notifications to"
+          chosen_pick('#account_timezone_id_chosen', text: @tz.display_name)
+        }
       }
     end
 
     it 'creates new account succesfully' do
       click_on_submit
 
-      p System::Timezone.all.inspect
-      p Contractor.all.inspect
-
-
       expect(page).to have_css('.flash_notice', text: 'Account was successfully created.')
 
       expect(Account.last).to have_attributes(
-                                     name: attributes[:name],
-                                     contractor_id: @customer.id,
-                                     max_balance: attributes[:max_balance],
-                                     min_balance: attributes[:min_balance],
-                                     balance_low_threshold: attributes[:balance_low_threshold],
-                                     balance_high_threshold: attributes[:balance_high_threshold],
-                                     origination_capacity: attributes[:origination_capacity],
-                                     termination_capacity: attributes[:termination_capacity],
-                                     timezone_id: @tz.id
-                                 )
+        name: attributes[:name],
+        contractor_id: @customer.id,
+        max_balance: attributes[:max_balance],
+        min_balance: attributes[:min_balance],
+        balance_low_threshold: attributes[:balance_low_threshold],
+        balance_high_threshold: attributes[:balance_high_threshold],
+        origination_capacity: attributes[:origination_capacity],
+        termination_capacity: attributes[:termination_capacity],
+        timezone_id: @tz.id
+      )
     end
   end
 
