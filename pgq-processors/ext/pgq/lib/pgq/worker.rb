9617df7719dacf5695210f9e751ed1dfeb104cf0
Denis Talakevich <senid231@gmail.com>
2018-12-17 15:42:21 +0200

    #415 pgq processor exit code 1 on error

--- pgq-processors/ext/pgq/lib/pgq/worker.rb
+++ pgq-processors/ext/pgq/lib/pgq/worker.rb
@@ -2,10 +2,17 @@ class Pgq::Worker
   attr_reader :logger, :class, :sleep_time
 
   cattr_accessor :interrupted do false end
+  cattr_accessor :graceful_exit do true end
   cattr_accessor :logger
 
   def self.shutdown!(signal)
-     self.interrupted = signal
+    if signal.is_a?(StandardError)
+      self.interrupted = "<#{signal.class}> #{signal.message}"
+      self.graceful_exit = false
+    else
+      self.interrupted = signal
+      self.graceful_exit = true
+    end
   end
 
   def self.interrupted?
@@ -46,9 +53,10 @@ class Pgq::Worker
 
 
   def self.check_interrupted(file, line)
-    if self.interrupted?
-        logger.info  "[#{file}: #{line}] Worker has been halted with #{self.interrupted}"
-        exit(0)
+    if interrupted?
+      exit_code = graceful_exit ? 0 : 1
+      logger.info { "[#{file}: #{line}] {exit_code:#{exit_code}} Worker has been halted with #{interrupted}" }
+      exit(exit_code)
     end
   end
 
