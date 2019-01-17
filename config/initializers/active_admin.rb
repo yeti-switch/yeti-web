5a6749a2d61d85e1c74e52d208c088182188e1c6
Denis Talakevich <senid231@gmail.com>
2019-01-17 14:03:19 +0200

    fix policy class and action detection when omitting action or subject

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -210,12 +210,27 @@ ActiveAdmin.setup do |config|
     # authorized? => authorized?(params[:action].to_sym, resource)
     # authorized?(record) => authorized?(params[:action].to_sym, record)
     def authorized?(action = nil, subject = nil)
+      action, subject = normalize_authorized_params(action, subject)
+      active_admin_authorization.authorized?(action, subject)
+    end
+
+    def authorize!(action = nil, subject = nil)
+      action, subject = normalize_authorized_params(action, subject)
+      unless authorized?(action, subject)
+        raise ActiveAdmin::AccessDenied.new(current_active_admin_user, action, subject)
+      end
+    end
+
+    private
+
+    def normalize_authorized_params(action, subject)
       if subject.nil? && (!action.is_a?(Symbol) && !action.is_a?(String) && !action.is_a?(NilClass))
         subject = action
         action = nil
       end
       action = params[:action].to_sym if action.nil?
-      active_admin_authorization.authorized?(action, subject)
+      subject = resource_class if subject.nil?
+      [action, subject]
     end
 
     private
