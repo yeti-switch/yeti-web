998070604ce9ec3f43a2ff482518eca689fa1b8a
Max Konin <maxim21214@gmail.com>
2019-03-28 12:57:29 +0700

    filter builder service

---
+++ app/services/ransack_filter_builder.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+class RansackFilterBuilder
+  RANSACK_ARRAY_SUFFIXES = %w[in not_in cont_any].freeze
+
+  def initialize(attr:, operator:)
+    @attr = attr
+    @operator = operator
+  end
+
+  def apply(records, value)
+    records.ransack(filter_name => value)
+  end
+
+  def filter_name
+    "#{@attr}_#{@operator}"
+  end
+
+  def verify(values)
+    return values if RANSACK_ARRAY_SUFFIXES.include?(@operator)
+    raise JSONAPI::Exceptions::InvalidFilterValue.new(filter_name, values.join(',')) if values.size != 1
+
+    values.first
+  end
+end
---
+++ spec/services/ransack_filter_builder_spec.rb
@@ -0,0 +1,58 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe RansackFilterBuilder do
+  let(:builder) { RansackFilterBuilder.new(attr: attr, operator: operator) }
+  let(:attr) { 'name' }
+
+  describe '#verify' do
+    let(:values) { [1, 2] }
+    subject { builder.verify(values) }
+
+    context 'when operator is "in"' do
+      let(:operator) { 'in' }
+      it { is_expected.to eq values }
+    end
+    context 'when operator is "not_in"' do
+      let(:operator) { 'not_in' }
+      it { is_expected.to eq values }
+    end
+    context 'when operator is "not_in"' do
+      let(:operator) { 'cont_any' }
+      it { is_expected.to eq values }
+    end
+    context 'when operator is not array ransack operator' do
+      let(:operator) { 'eq' }
+      context 'when value.size !== 1' do
+        it { expect { subject }.to raise_error(JSONAPI::Exceptions::InvalidFilterValue) }
+      end
+      context 'when value.size == 1' do
+        let(:values) { [1] }
+        it { is_expected.to eq values.first }
+      end
+    end
+  end
+
+  describe '#apply' do
+    let(:records) { double 'records' }
+    let(:ransack_result) { double 'ransack_result' }
+    let(:operator) { 'eq' }
+    let(:value) { 'str' }
+
+    before do
+      allow(ransack_result).to receive(:results)
+      expect(records).to receive(:ransack).with('name_eq' => value).and_return(ransack_result)
+    end
+
+    it 'builds correct ransack operator' do
+      builder.apply(records, value)
+    end
+  end
+
+  describe '#filter_name' do
+    let(:operator) { 'eq' }
+    subject { builder.filter_name }
+    it { is_expected.to eq "#{attr}_eq" }
+  end
+end
