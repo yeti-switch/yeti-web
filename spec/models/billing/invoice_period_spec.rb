079d8879e41c016e56a31358ccf760e881c8b146
Denis Talakevich <senid231@gmail.com>
2019-06-01 15:50:18 +0300

    refactor invoice_period_spec

--- spec/models/billing/invoice_period_spec.rb
+++ spec/models/billing/invoice_period_spec.rb
@@ -9,21 +9,24 @@ describe Billing::InvoicePeriod do
 
   let(:id) { Billing::InvoicePeriod::NAMES.key(name.upcase) }
 
-  shared_examples :should_set_correct_dates do
-    let(:dt) { nil }
-    let(:expected_next_dt) { nil }
-    let(:expected_initial_dt) { nil }
-    let(:expected_next_from_now_dt) { expected_next_dt }
-    before { allow(Billing::InvoicePeriod).to receive(:today).and_return dt.to_date }
-    it 'should return correct next_date' do
-      expect(subject.next_date(dt).to_time.to_s(:db)).to eq expected_next_dt.to_time.to_s(:db)
-    end
-    it 'should return correct next_date_from_now' do
-      expect(subject.next_date_from_now.to_time.to_s(:db)).to eq expected_next_from_now_dt.to_time.to_s(:db)
-    end
-    it 'should return correct initial_date' do
-      end_date = Time.now.to_date
-      expect(subject.initial_date(end_date).to_time.to_time.to_s(:db)).to eq expected_initial_dt.to_time.to_s(:db)
+  shared_examples :responds_with_correct_times do |today_dt:, next_dt:, initial_dt:, next_from_now_dt: nil|
+    next_from_now_dt ||= next_dt
+
+    context "today is #{today_dt.to_date}" do
+      it "#next_date for today equals '#{next_dt}'" do
+        result = travel_to(today_dt) { subject.next_date(today_dt) }
+        expect(result.to_time.to_s(:db)).to eq next_dt.to_time.to_s(:db)
+      end
+
+      it "#next_date_from_now equals '#{next_from_now_dt}'" do
+        result = travel_to(today_dt) { subject.next_date_from_now }
+        expect(result.to_time.to_s(:db)).to eq next_from_now_dt.to_time.to_s(:db)
+      end
+
+      it "#initial_date equals '#{initial_dt}'" do
+        result = travel_to(today_dt) { subject.initial_date(Time.now.to_date) }
+        expect(result.to_time.to_s(:db)).to eq initial_dt.to_time.to_s(:db)
+      end
     end
   end
 
@@ -31,28 +34,25 @@ describe Billing::InvoicePeriod do
     let(:name) { 'weekly' }
 
     context 'when invoice did not break month' do
-      include_examples :should_set_correct_dates do
-        let(:dt) { Time.parse('2015-07-06 00:00:00') }
-        let(:expected_next_dt) { dt + 1.week }
-        let(:expected_initial_dt) { dt.to_time }
-      end
+      include_examples :responds_with_correct_times,
+                       today_dt: Time.parse('2015-07-06 00:00:00'),
+                       next_dt: Time.parse('2015-07-13 00:00:00'),
+                       initial_dt: Time.parse('2015-07-06 00:00:00')
     end
 
     context 'when invoice breaks month' do
-      include_examples :should_set_correct_dates do
-        let(:dt) { Time.parse('2015-07-27 00:00:00') }
-        let(:expected_next_dt) { dt + 1.week }
-        let(:expected_initial_dt) { dt.to_time }
-      end
+      include_examples :responds_with_correct_times,
+                       today_dt: Time.parse('2015-07-27 00:00:00'),
+                       next_dt: Time.parse('2015-08-03 00:00:00'),
+                       initial_dt: Time.parse('2015-07-27 00:00:00')
     end
 
     context 'when invoice on last week of the year' do
-      include_examples :should_set_correct_dates do
-        let(:dt) { Time.parse('2017-12-27 00:00:00') }
-        let(:expected_next_dt) { dt + 1.week }
-        let(:expected_initial_dt) { dt.beginning_of_week }
-        let(:expected_next_from_now_dt) { expected_next_dt.beginning_of_week }
-      end
+      include_examples :responds_with_correct_times,
+                       today_dt: Time.parse('2017-12-27 00:00:00'),
+                       next_dt: Time.parse('2018-01-03 00:00:00'),
+                       initial_dt: Time.parse('2017-12-25 00:00:00'),
+                       next_from_now_dt: Time.parse('2018-01-01 00:00:00')
     end
   end # WEEKLY
 
@@ -60,27 +60,25 @@ describe Billing::InvoicePeriod do
     let(:name) { 'weekly_split' }
 
     context 'when invoice did not break month' do
-      include_examples :should_set_correct_dates do
-        let(:dt) { Time.parse('2015-07-06 00:00:00') }
-        let(:expected_next_dt) { dt.to_time + 1.week }
-        let(:expected_initial_dt) { dt.to_time }
-      end
+      include_examples :responds_with_correct_times,
+                       today_dt: Time.parse('2015-07-06 00:00:00'),
+                       next_dt: Time.parse('2015-07-13 00:00:00'),
+                       initial_dt: Time.parse('2015-07-06 00:00:00')
     end
 
     context 'when invoice breaks month' do
       context 'first part' do
-        include_examples :should_set_correct_dates do
-          let(:dt) { Time.parse('2015-07-27 00:00:00') }
-          let(:expected_next_dt) { Time.parse('2015-08-01 00:00:00') }
-          let(:expected_initial_dt) { dt.to_time }
-        end
+        include_examples :responds_with_correct_times,
+                         today_dt: Time.parse('2015-07-27 00:00:00'),
+                         next_dt: Time.parse('2015-08-01 00:00:00'),
+                         initial_dt: Time.parse('2015-07-27 00:00:00')
       end
+
       context 'second part' do
-        include_examples :should_set_correct_dates do
-          let(:dt) { Time.parse('2015-08-01 00:00:00') }
-          let(:expected_next_dt) { Time.parse('2015-08-03 00:00:00') }
-          let(:expected_initial_dt) { dt.to_time }
-        end
+        include_examples :responds_with_correct_times,
+                         today_dt: Time.parse('2015-08-01 00:00:00'),
+                         next_dt: Time.parse('2015-08-03 00:00:00'),
+                         initial_dt: Time.parse('2015-08-01 00:00:00')
       end
     end
   end # WEEKLY_SPLIT
@@ -90,59 +88,53 @@ describe Billing::InvoicePeriod do
 
     context 'even week' do
       context 'when invoice did not break month' do
-        include_examples :should_set_correct_dates do
-          let(:dt) { Time.parse('2015-07-06 00:00:00') } # week 28
-          let(:expected_next_dt) { dt + 2.week }
-          let(:expected_initial_dt) { dt }
-        end
+        include_examples :responds_with_correct_times,
+                         today_dt: Time.parse('2015-07-06 00:00:00'), # week 28
+                         next_dt: Time.parse('2015-07-20 00:00:00'),
+                         initial_dt: Time.parse('2015-07-06 00:00:00')
       end
 
       context 'when invoice breaks month' do
         context 'when breaks first week' do
-          include_examples :should_set_correct_dates do
-            let(:dt) { Time.parse('2015-04-27 00:00:00') } # week 18
-            let(:expected_next_dt) { dt + 2.week }
-            let(:expected_initial_dt) { dt }
-          end
+          include_examples :responds_with_correct_times,
+                           today_dt: Time.parse('2015-04-27 00:00:00'), # week 18
+                           next_dt: Time.parse('2015-05-11 00:00:00'),
+                           initial_dt: Time.parse('2015-04-27 00:00:00')
         end
 
         context 'when breaks second week' do
-          include_examples :should_set_correct_dates do
-            let(:dt) { Time.parse('2015-06-22 00:00:00') } # week 26
-            let(:expected_next_dt) { dt + 2.week }
-            let(:expected_initial_dt) { dt }
-          end
+          include_examples :responds_with_correct_times,
+                           today_dt: Time.parse('2015-06-22 00:00:00'), # week 26
+                           next_dt: Time.parse('2015-07-06 00:00:00'),
+                           initial_dt: Time.parse('2015-06-22 00:00:00')
         end
       end
     end # even week
 
     context 'odd week' do
       context 'when invoice did not break month' do
-        include_examples :should_set_correct_dates do
-          let(:dt) { Time.parse('2015-07-13 00:00:00') } # week 29
-          let(:expected_next_dt) { dt + 2.week }
-          let(:expected_next_from_now_dt) { dt + 1.week }
-          let(:expected_initial_dt) { dt - 1.week }
-        end
+        include_examples :responds_with_correct_times,
+                         today_dt: Time.parse('2015-07-13 00:00:00'), # week 29
+                         next_dt: Time.parse('2015-07-27 00:00:00'),
+                         initial_dt: Time.parse('2015-07-06 00:00:00'),
+                         next_from_now_dt: Time.parse('2015-07-20 00:00:00')
       end
 
       context 'when invoice breaks month' do
         context 'when breaks first week' do
-          include_examples :should_set_correct_dates do
-            let(:dt) { Time.parse('2015-06-29 00:00:00') } # week 27
-            let(:expected_next_dt) { dt + 2.week }
-            let(:expected_next_from_now_dt) { dt + 1.week }
-            let(:expected_initial_dt) { dt - 1.week }
-          end
+          include_examples :responds_with_correct_times,
+                           today_dt: Time.parse('2015-06-29 00:00:00'), # week 27
+                           next_dt: Time.parse('2015-07-13 00:00:00'),
+                           initial_dt: Time.parse('2015-06-22 00:00:00'),
+                           next_from_now_dt: Time.parse('2015-07-06 00:00:00')
         end
 
         context 'whn breaks second week' do
-          include_examples :should_set_correct_dates do
-            let(:dt) { Time.parse('2015-04-20 00:00:00') } # week 17
-            let(:expected_next_dt) { dt + 2.week }
-            let(:expected_next_from_now_dt) { dt + 1.week }
-            let(:expected_initial_dt) { dt - 1.week }
-          end
+          include_examples :responds_with_correct_times,
+                           today_dt: Time.parse('2015-04-20 00:00:00'), # week 17
+                           next_dt: Time.parse('2015-05-04 00:00:00'),
+                           initial_dt: Time.parse('2015-04-13 00:00:00'),
+                           next_from_now_dt: Time.parse('2015-04-27 00:00:00')
         end
       end
     end # odd week
@@ -153,47 +145,42 @@ describe Billing::InvoicePeriod do
 
     context 'even week' do
       context 'when invoice did not break month' do
-        include_examples :should_set_correct_dates do
-          let(:dt) { Time.parse('2015-07-06 00:00:00') } # week 28
-          let(:expected_next_dt) { dt + 2.week }
-          let(:expected_initial_dt) { dt }
-        end
+        include_examples :responds_with_correct_times,
+                         today_dt: Time.parse('2015-07-06 00:00:00'), # week 28
+                         next_dt: Time.parse('2015-07-20 00:00:00'),
+                         initial_dt: Time.parse('2015-07-06 00:00:00')
       end
 
       context 'when invoice breaks month' do
         context 'when breaks first week' do
           context 'first part' do
-            include_examples :should_set_correct_dates do
-              let(:dt) { Time.parse('2015-04-27 00:00:00') } # week 18
-              let(:expected_next_dt) { Time.parse('2015-05-01 00:00:00') } # week 18
-              let(:expected_initial_dt) { dt }
-            end
+            include_examples :responds_with_correct_times,
+                             today_dt: Time.parse('2015-04-27 00:00:00'), # week 18
+                             next_dt: Time.parse('2015-05-01 00:00:00'), # week 18
+                             initial_dt: Time.parse('2015-04-27 00:00:00')
           end
 
           context 'second part' do
-            include_examples :should_set_correct_dates do
-              let(:dt) { Time.parse('2015-05-01 00:00:00') } # week 18
-              let(:expected_next_dt) { Time.parse('2015-05-11 00:00:00') } # week 20
-              let(:expected_initial_dt) { dt }
-            end
+            include_examples :responds_with_correct_times,
+                             today_dt: Time.parse('2015-05-01 00:00:00'), # week 18
+                             next_dt: Time.parse('2015-05-11 00:00:00'), # week 20
+                             initial_dt: Time.parse('2015-05-01 00:00:00')
           end
         end
 
         context 'when breaks second week' do
           context 'second part' do
-            include_examples :should_set_correct_dates do
-              let(:dt) { Time.parse('2015-06-22 00:00:00') } # week 26
-              let(:expected_next_dt) { Time.parse('2015-07-01 00:00:00') } # week 27
-              let(:expected_initial_dt) { dt }
-            end
+            include_examples :responds_with_correct_times,
+                             today_dt: Time.parse('2015-06-22 00:00:00'), # week 26
+                             next_dt: Time.parse('2015-07-01 00:00:00'), # week 27
+                             initial_dt: Time.parse('2015-06-22 00:00:00')
           end
 
           context 'second part' do
-            include_examples :should_set_correct_dates do
-              let(:dt) { Time.parse('2015-07-01 00:00:00') } # week 27
-              let(:expected_next_dt) { Time.parse('2015-07-06 00:00:00') } # week 28
-              let(:expected_initial_dt) { dt }
-            end
+            include_examples :responds_with_correct_times,
+                             today_dt: Time.parse('2015-07-01 00:00:00'), # week 27
+                             next_dt: Time.parse('2015-07-06 00:00:00'), # week 28
+                             initial_dt: Time.parse('2015-07-01 00:00:00')
           end
         end
       end
@@ -201,43 +188,39 @@ describe Billing::InvoicePeriod do
 
     context 'odd week' do
       context 'when invoice did not break month' do
-        include_examples :should_set_correct_dates do
-          let(:dt) { Time.parse('2015-07-13 00:00:00') } # week 29
-          # cause it will move to the even week
-          let(:expected_next_dt) { dt + 1.week }
-          let(:expected_initial_dt) { dt - 1.week }
-        end
+        include_examples :responds_with_correct_times,
+                         today_dt: Time.parse('2015-07-13 00:00:00'), # week 29
+                         # cause it will move to the even week
+                         next_dt: Time.parse('2015-07-20 00:00:00'),
+                         initial_dt: Time.parse('2015-07-06 00:00:00')
       end
 
       context 'when invoice breaks month' do
         context 'when breaks first week' do
           context 'first part' do
-            include_examples :should_set_correct_dates do
-              let(:dt) { Time.parse('2015-06-29 00:00:00') } # week 27
-              let(:expected_next_dt) { Time.parse('2015-07-01 00:00:00') } # week 27
-              # cause it will move to the even week
-              let(:expected_initial_dt) { Time.parse('2015-06-22 00:00:00') } # week 26
-            end
+            include_examples :responds_with_correct_times,
+                             today_dt: Time.parse('2015-06-29 00:00:00'), # week 27
+                             next_dt: Time.parse('2015-07-01 00:00:00'), # week 27
+                             # cause it will move to the even week
+                             initial_dt: Time.parse('2015-06-22 00:00:00') # week 26
           end
 
           context 'second part' do
-            include_examples :should_set_correct_dates do
-              let(:dt) { Time.parse('2015-07-01 00:00:00') } # week 27
-              # cause it will move to the even week
-              let(:expected_next_dt) { Time.parse('2015-07-06 00:00:00') } # week 28
-              let(:expected_initial_dt) { dt }
-            end
+            include_examples :responds_with_correct_times,
+                             today_dt: Time.parse('2015-07-01 00:00:00'), # week 27
+                             # cause it will move to the even week
+                             next_dt: Time.parse('2015-07-06 00:00:00'), # week 28
+                             initial_dt: Time.parse('2015-07-01 00:00:00')
           end
         end
 
         context 'when breaks second week' do
           context 'first part' do
-            include_examples :should_set_correct_dates do
-              let(:dt) { Time.parse('2015-04-20 00:00:00') } # week 17
-              # cause it will move to the even week
-              let(:expected_next_dt) { Time.parse('2015-04-27 00:00:00') } # week 18
-              let(:expected_initial_dt) { Time.parse('2015-04-13 00:00:00') } # week 18
-            end
+            include_examples :responds_with_correct_times,
+                             today_dt: Time.parse('2015-04-20 00:00:00'), # week 17
+                             # cause it will move to the even week
+                             next_dt: Time.parse('2015-04-27 00:00:00'), # week 18
+                             initial_dt: Time.parse('2015-04-13 00:00:00') # week 18
           end
         end
       end
