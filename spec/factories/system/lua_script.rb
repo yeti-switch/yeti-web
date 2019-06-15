05aee9a4ac9f1dbab0ad92de9a63640de1a901a2
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2019-06-15 20:03:24 +0300

    fix lua script factory

--- spec/factories/system/lua_script.rb
+++ spec/factories/system/lua_script.rb
@@ -2,7 +2,7 @@
 
 FactoryGirl.define do
   factory :lua_script, class: System::LuaScript do
-    name 'test LUAs'
+    sequence(:name) { |n| "LUA script_#{n}" }
     source 'arg.a="000"; table.insert(arg.v,9); return arg;'
   end
 end
