3d7f410c2628a75b53f8ec4045b50e4d70d0069e
Denis Talakevich <senid231@gmail.com>
2019-08-26 12:51:49 +0300

    add tests for realtime data admin UI index pages

---
+++ spec/features/realtime_data/outgoing_resgistrations/index_outgoing_registrations_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Index Outgoing Registrations', type: :feature do
+  include_context :login_as_admin
+
+  let!(:node) { FactoryGirl.create(:node) }
+  let(:record_attributes) { FactoryGirl.attributes_for(:outgoing_registration, :filled) }
+  before do
+    stub_jrpc_request('show.registrations', node.rpc_endpoint).and_return([record_attributes.stringify_keys])
+    visit outgoing_registrations_path(q: { node_id_eq: node.id })
+  end
+
+  it 'has record' do
+    expect(page).to have_css('.col-id', text: record_attributes[:id])
+    expect(page).to_not have_css('flash-warning')
+    expect(page).to_not have_css('flash-error')
+  end
+end
