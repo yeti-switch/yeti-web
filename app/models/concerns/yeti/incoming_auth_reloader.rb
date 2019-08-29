d7de8d355a2d4c56710ee01fafe04dab3717d1de
Denis Talakevich <senid231@gmail.com>
2019-08-29 22:39:10 +0300

    gateway trigger request.auth.credentials.reload on conditions
    fixes #521

--- app/models/concerns/yeti/incoming_auth_reloader.rb
+++ app/models/concerns/yeti/incoming_auth_reloader.rb
@@ -4,13 +4,30 @@ module Yeti
   module IncomingAuthReloader
     extend ActiveSupport::Concern
     included do
-      before_save do
-        Event.reload_incoming_auth
-      end
+      before_create :reload_incoming_auth!, if: :reload_incoming_auth_on_create?
+      before_update :reload_incoming_auth!, if: :reload_incoming_auth_on_update?
+      before_destroy :reload_incoming_auth!, if: :reload_incoming_auth_on_destroy?
 
-      before_destroy do
-        Event.reload_incoming_auth
-      end
+      private :reload_incoming_auth!,
+              :reload_incoming_auth_on_create?,
+              :reload_incoming_auth_on_update?,
+              :reload_incoming_auth_on_destroy?
+    end
+
+    def reload_incoming_auth!
+      Event.reload_incoming_auth
+    end
+
+    def reload_incoming_auth_on_create?
+      true
+    end
+
+    def reload_incoming_auth_on_update?
+      true
+    end
+
+    def reload_incoming_auth_on_destroy?
+      true
     end
   end
 end
--- spec/support/examples/active_record/does_not_update_record.rb
+++ spec/support/examples/active_record/does_not_update_record.rb
@@ -1,11 +1,12 @@
 # frozen_string_literal: true
 
 RSpec.shared_examples :does_not_update_record do |errors: {}|
+  # requires let(:record) to specified
   let(:expected_record_errors) { errors }
 
   it 'does not update record' do
     expect(subject).to eq(false)
     expected_errors = expected_record_errors.map { |k, v| [k, Array.wrap(v)] }.to_h
-    expect(subject.errors.messages).to match(expected_errors)
+    expect(record.errors.messages).to match(expected_errors)
   end
 end
