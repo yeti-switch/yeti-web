ffa3e7ece4fbe25af6e397115cb51b44eb220400
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2019-10-23 00:29:06 +0300

    fix process_dp SP

--- config/initializers/pg_dump.rb
+++ config/initializers/pg_dump.rb
@@ -3,7 +3,8 @@
 ActiveRecord::Tasks::DatabaseTasks.structure_dump_flags = [
   '-T', 'cdr.cdr_2*',
   '-T', 'auth_log.auth_log_2*',
-  '-T', 'rtp_statistics.streams_*',
+  '-T', 'rtp_statistics.streams_2*',
+  '-T', 'logs.api_requests_2*',
   '-T', 'pgq.*',
   '-T', 'pgq_ext.*'
 ]
