f953e222b60d7bf4f95c1f939d1e67bdbe14df36
Denis Talakevich <senid231@gmail.com>
2019-12-16 21:34:35 +0200

    fix policies for acts_as_status

--- app/policies/routing/destination_policy.rb
+++ app/policies/routing/destination_policy.rb
@@ -6,7 +6,7 @@ module Routing
 
     alias_rule :clear_quality_alarm?, to: :perform?
     alias_rule :truncate_short_window_stats?, to: :perform? # DSL acts_as_quality_stat
-    alias_rule :enabled?, :disabled?, to: :perform? # DSL acts_as_status
+    alias_rule :enable?, :disable?, to: :perform? # DSL acts_as_status
 
     class Scope < ::RolePolicy::Scope
     end
