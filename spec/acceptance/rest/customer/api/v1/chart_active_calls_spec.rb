e10fcb8a4b75c6d498510e6e32e84a314e2282df
Denis Talakevich <senid231@gmail.com>
2020-01-21 19:52:11 +0200

    add acceptance tests for chart-active-calls and chart-originated-cps

---
+++ spec/acceptance/rest/customer/api/v1/chart_active_calls_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+require 'rspec_api_documentation/dsl'
+
+resource 'ChartActiveCalls', document: :customer_v1 do
+  header 'Accept', 'application/vnd.api+json'
+  header 'Content-Type', 'application/vnd.api+json'
+  header 'Authorization', :auth_token
+
+  let(:api_access) { create :api_access }
+  let(:customer) { api_access.customer }
+  let(:auth_token) { ::Knock::AuthToken.new(payload: { sub: api_access.id }).token }
+  let(:type) { 'chart-active-calls' }
+
+  let!(:customer_acc) { create(:account, contractor: customer).reload }
+
+  before do
+    create :active_call_account,
+           account: customer_acc,
+           originated_count: 10,
+           terminated_count: 15,
+           created_at: '2019-01-01 00:00:01'
+  end
+
+  post '/api/rest/customer/v1/chart-active-calls' do
+    parameter :type, 'Resource type (chart-active-calls)', scope: :data, required: true
+
+    required_params = %i[from-time to-time]
+    required_relations = %i[account]
+
+    jsonapi_attributes(required_params, [])
+    jsonapi_relationships(required_relations, [])
+
+    let(:'from-time') { '2019-01-01 00:00:00' }
+    let(:'to-time') { '2019-01-02 00:00:00' }
+    let(:account) { wrap_relationship(:accounts, customer_acc.uuid) }
+
+    example_request 'get active calls chart for account during time period' do
+      expect(status).to eq(201)
+    end
+  end
+end
