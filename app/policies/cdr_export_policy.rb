2ceab6fc34244d719ef16e6002153cf4a9c72526
Denis Talakevich <senid231@gmail.com>
2020-02-17 12:43:06 +0200

    fix admin UI policy for CDR Export delete_file

--- app/policies/cdr_export_policy.rb
+++ app/policies/cdr_export_policy.rb
@@ -4,6 +4,7 @@ class CdrExportPolicy < ::RolePolicy
   section 'CdrExport'
 
   alias_rule :download?, to: :perform?
+  alias_rule :delete_file?, to: :destroy?
 
   class Scope < ::RolePolicy::Scope
   end
