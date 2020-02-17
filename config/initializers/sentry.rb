d48e8b3053ef14a6a2a679c11f16a6f0e1d3b109
Denis Talakevich <senid231@gmail.com>
2020-02-17 12:39:21 +0200

    fix sentry for active_admin pages

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -5,3 +5,17 @@ require 'capture_error'
 if CaptureError.enabled? && !defined?(::Rake)
   CaptureError.configure!
 end
+
+ActiveAdmin::BaseController.class_eval do
+  include CaptureError::ControllerMethods
+
+  def capture_user
+    return if current_admin_user.nil?
+
+    {
+      id: current_admin_user.id,
+      username: current_admin_user.username,
+      class: 'AdminUser'
+    }
+  end
+end
