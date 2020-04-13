66789f4073581875891347435ebc7606431dcb68
Igor Gonchar <igor.gonchar@gmail.com>
2020-04-13 11:37:50 +0300

    coverage merge results
    https://docs.coveralls.io/ruby-on-rails 
    MERGING MULTIPLE TEST SUITES

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear_merged!
 
 require File.expand_path('../config/environment', __dir__)
 require 'rspec/rails'
