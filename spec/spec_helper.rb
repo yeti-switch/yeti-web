85ccb4334c577654546222cc5e888aff8c31fb0f
Denis Talakevich <senid231@gmail.com>
2020-04-20 14:19:46 +0300

    some changes to start CI 2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,6 +10,7 @@ require 'rspec/rails'
 require 'webmock/rspec'
 require 'pundit/rspec'
 WebMock.disable_net_connect!(allow_localhost: true)
+# require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
