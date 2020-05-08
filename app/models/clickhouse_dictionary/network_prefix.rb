465272039e627e5131d3b6ecb69314e9c7aa46c8
Ivanov-Anton <jinsike123@gmail.com>
2020-05-08 12:19:11 +0000

    used Arel wrapping to avoid SQL warning

--- app/models/clickhouse_dictionary/network_prefix.rb
+++ app/models/clickhouse_dictionary/network_prefix.rb
@@ -4,11 +4,11 @@ module ClickhouseDictionary
   class NetworkPrefix < Base
     model_class ::System::NetworkPrefix
 
-    attributes :id,
-               :prefix,
+    attributes :prefix,
                :country_id,
                :network_id
 
+    attribute :id, sql: 'sys.network_prefixes.id'
     attribute :country_name, sql: 'sys.countries.name'
     attribute :network_name, sql: 'sys.networks.name'
 
