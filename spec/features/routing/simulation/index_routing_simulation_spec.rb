ca5f53f9fbf43a7bc8bf6205ab8b0d9ec47707e6
Anton <jinsike123@gmail.com>
2020-07-06 10:29:43 +0000

    closes #757 undefined local variable pop_id for Routing::Simulation:Class

---
+++ spec/features/routing/simulation/index_routing_simulation_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+RSpec.describe Routing::Simulation, 'index' do
+  include_context :login_as_admin
+  subject { visit routing_simulation_path }
+
+  it 'should load Simulation page' do
+    subject
+    expect(page).to have_content 'Routing simulation'
+    expect(page).to have_current_path '/routing_simulation'
+    expect(page).to_not have_css 'flash-warning'
+    expect(page).to_not have_css 'flash-error'
+  end
+end
---
+++ spec/models/routing/simulation_spec.rb
@@ -0,0 +1,55 @@
+# frozen_string_literal: true
+
+RSpec.describe Routing::Simulation do
+  describe 'validates' do
+    context '#remote_ip' do
+      it { is_expected.to_not allow_value('').for :remote_ip }
+
+      it { is_expected.to allow_value('127.0.0.1').for :remote_ip }
+    end
+
+    context '#remote_port' do
+      it { is_expected.to_not allow_value('').for :remote_port }
+
+      it { is_expected.to allow_value(Yeti::ActiveRecord::L4_PORT_MIN + 10).for :remote_port }
+    end
+
+    context '#src_number' do
+      it { is_expected.to_not allow_value('').for :src_number }
+
+      it { is_expected.to allow_value(3).for :src_number }
+    end
+
+    context '#dst_number' do
+      it { is_expected.to_not allow_value('').for :dst_number }
+
+      it { is_expected.to allow_value(4).for :dst_number }
+    end
+
+    context '#pop_id' do
+      it { is_expected.to_not allow_value('').for :pop_id }
+      it { is_expected.to_not allow_value('string').for :pop_id }
+
+      it { is_expected.to allow_value(3).for :pop_id }
+    end
+
+    context '#transport_protocol_id' do
+      it { is_expected.to_not allow_value('').for :transport_protocol_id }
+      it { is_expected.to_not allow_value('string').for :transport_protocol_id }
+
+      it { is_expected.to allow_value(Equipment::TransportProtocol.take!.id).for :transport_protocol_id }
+    end
+
+    context '#remote_port' do
+      it { is_expected.to_not allow_value('').for :remote_port }
+      it { is_expected.to_not allow_value('string').for :remote_port }
+      it { is_expected.to_not allow_value(Yeti::ActiveRecord::L4_PORT_MIN - 1).for :remote_port }
+      it { is_expected.to_not allow_value(Yeti::ActiveRecord::L4_PORT_MAX + 1).for :remote_port }
+      it { is_expected.to_not allow_value(Yeti::ActiveRecord::L4_PORT_MAX).for :remote_port }
+      it { is_expected.to_not allow_value(2.5).for :remote_port }
+
+      it { is_expected.to allow_value(Yeti::ActiveRecord::L4_PORT_MIN).for :remote_port }
+      it { is_expected.to allow_value(Yeti::ActiveRecord::L4_PORT_MAX - 1).for :remote_port }
+    end
+  end
+end
