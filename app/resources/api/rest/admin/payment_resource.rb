86d63a7384de7d749e24e681d970c2f4fea1f1fa
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2020-08-30 13:42:06 +0300

    disable payments deletion refs #758 (#777)
    * disable payments deletion refs #758

--- app/resources/api/rest/admin/payment_resource.rb
+++ app/resources/api/rest/admin/payment_resource.rb
@@ -8,15 +8,11 @@ class Api::Rest::Admin::PaymentResource < BaseResource
   ransack_filter :amount, type: :number
   ransack_filter :notes, type: :string
 
-  def self.updatable_fields(_context)
+  def self.creatable_fields(_context)
     %i[
       account
       amount
       notes
     ]
-  end
-
-  def self.creatable_fields(context)
-    updatable_fields(context)
   end
 end
