75552c1e85b5969736baf60f09bb8beedec5d20f
Dmitry <dmitry.sinina@onat.edu.ua>
2020-10-16 00:31:10 +0300

    structure

--- db/structure.sql
+++ db/structure.sql
@@ -42838,25 +42838,6 @@ CREATE TABLE sys.nodes (
 
 
 --
--- Name: node_id_seq; Type: SEQUENCE; Schema: sys; Owner: -
---
-
-CREATE SEQUENCE sys.node_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: node_id_seq; Type: SEQUENCE OWNED BY; Schema: sys; Owner: -
---
-
-ALTER SEQUENCE sys.node_id_seq OWNED BY sys.nodes.id;
-
-
---
 -- Name: pops; Type: TABLE; Schema: sys; Owner: -
 --
 
@@ -42864,25 +42845,6 @@ CREATE TABLE sys.pops (
     id integer NOT NULL,
     name character varying NOT NULL
 );
-
-
---
--- Name: pop_id_seq; Type: SEQUENCE; Schema: sys; Owner: -
---
-
-CREATE SEQUENCE sys.pop_id_seq
-    START WITH 1
-    INCREMENT BY 1
-    NO MINVALUE
-    NO MAXVALUE
-    CACHE 1;
-
-
---
--- Name: pop_id_seq; Type: SEQUENCE OWNED BY; Schema: sys; Owner: -
---
-
-ALTER SEQUENCE sys.pop_id_seq OWNED BY sys.pops.id;
 
 
 --
@@ -43802,20 +43764,6 @@ ALTER TABLE ONLY sys.network_types ALTER COLUMN id SET DEFAULT nextval('sys.netw
 --
 
 ALTER TABLE ONLY sys.networks ALTER COLUMN id SET DEFAULT nextval('sys.networks_id_seq'::regclass);
-
-
---
--- Name: nodes id; Type: DEFAULT; Schema: sys; Owner: -
---
-
-ALTER TABLE ONLY sys.nodes ALTER COLUMN id SET DEFAULT nextval('sys.node_id_seq'::regclass);
-
-
---
--- Name: pops id; Type: DEFAULT; Schema: sys; Owner: -
---
-
-ALTER TABLE ONLY sys.pops ALTER COLUMN id SET DEFAULT nextval('sys.pop_id_seq'::regclass);
 
 
 --
@@ -47094,6 +47042,7 @@ INSERT INTO "public"."schema_migrations" (version) VALUES
 ('20200504210442'),
 ('20200630132440'),
 ('20200803202810'),
-('20201015195346');
+('20201015195346'),
+('20201015202253');
 
 
