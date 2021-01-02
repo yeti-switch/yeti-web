23660c376eb3d4f3b0d41f0945c3b7e0b4527c9f
Denis Talakevich <senid231@gmail.com>
2021-01-02 15:06:02 +0200

    fix invoices generation at beginning of year

--- app/lib/billing_period/biweekly.rb
+++ app/lib/billing_period/biweekly.rb
@@ -6,7 +6,7 @@ module BillingPeriod
 
     def period_end_for(time)
       date = time.to_date
-      today_week_start = Date.commercial(date.year, date.cweek)
+      today_week_start = Date.commercial(date.cwyear, date.cweek)
 
       period_end_date = [
         today_week_start + 2.weeks,
@@ -18,7 +18,7 @@ module BillingPeriod
 
     def period_start_for(period_end)
       period_end_date = period_end.to_date
-      period_start_date = Date.commercial(period_end_date.year, period_end_date.cweek) - 2.weeks
+      period_start_date = Date.commercial(period_end_date.cwyear, period_end_date.cweek) - 2.weeks
 
       date_to_time_in_time_zone(period_start_date)
     end
--- app/lib/billing_period/weekly.rb
+++ app/lib/billing_period/weekly.rb
@@ -6,14 +6,14 @@ module BillingPeriod
 
     def period_end_for(time)
       date = time.to_date
-      period_end_date = Date.commercial(date.year, date.cweek) + 1.week
+      period_end_date = Date.commercial(date.cwyear, date.cweek) + 1.week
 
       date_to_time_in_time_zone(period_end_date)
     end
 
     def period_start_for(period_end)
       period_end_date = period_end.to_date
-      period_start_date = Date.commercial(period_end_date.year, period_end_date.cweek) - 1.week
+      period_start_date = Date.commercial(period_end_date.cwyear, period_end_date.cweek) - 1.week
 
       date_to_time_in_time_zone(period_start_date)
     end
