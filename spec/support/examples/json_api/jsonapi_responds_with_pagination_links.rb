623e02b0a3389f7eff2568dc468f536e18067c5e
paratskiy <bog4242@gmail.com>
2021-01-15 18:40:20 +0200

    add shared examples for testing pagination

---
+++ spec/support/examples/json_api/jsonapi_responds_with_pagination_links.rb
@@ -0,0 +1,9 @@
+RSpec.shared_examples :jsonapi_responds_with_pagination_links do
+  it 'has pagination' do
+    subject
+    expect(response_json[:links]).to match(
+                                       first: be_present,
+                                       last: be_present
+                                     )
+  end
+end
