f6946af16a7e7a08c422f10cbccf9e229204d42d
paratskiy <bog4242@gmail.com>
2021-01-19 16:03:31 +0200

    rewrite clickhouse_dictionaries tests with new helper

--- spec/requests/api/rest/clickhouse_dictionaries/gateways_spec.rb
+++ spec/requests/api/rest/clickhouse_dictionaries/gateways_spec.rb
@@ -1,30 +1,30 @@
 # frozen_string_literal: true
 
-RSpec.describe '/api/rest/clickhouse-dictionaries/gateways' do
-  subject do
-    get clickhouse_dictionary_path
-  end
+RSpec.describe Api::Rest::ClickhouseDictionaries::GatewaysController do
+  include_context :clickhouse_dictionaries_api_helpers, type: :gateways
 
-  include_context :clickhouse_dictionaries_api_helpers do
-    let(:clickhouse_dictionary_type) { 'gateways' }
-  end
+  describe 'GET /api/rest/clickhouse-dictionaries/gateways' do
+    subject do
+      get clickhouse_dictionary_request_path
+    end
 
-  let!(:gateways) { create_list(:gateway, 3) + [create(:gateway, external_id: 123)] }
+    let!(:gateways) { create_list(:gateway, 3) + [create(:gateway, external_id: 123)] }
 
-  include_examples :responds_with_correct_json_each_row do
-    let(:expected_rows) do
-      gateways.map do |record|
-        {
-          id: record.id,
-          name: record.name,
-          external_id: record.external_id,
-          origination_capacity: record.origination_capacity,
-          termination_capacity: record.termination_capacity,
-          enabled: record.enabled,
-          gateway_group_id: record.gateway_group_id,
-          allow_origination: record.allow_origination,
-          allow_termination: record.allow_termination
-        }
+    include_examples :responds_with_correct_json_each_row do
+      let(:expected_rows) do
+        gateways.map do |record|
+          {
+            id: record.id,
+            name: record.name,
+            external_id: record.external_id,
+            origination_capacity: record.origination_capacity,
+            termination_capacity: record.termination_capacity,
+            enabled: record.enabled,
+            gateway_group_id: record.gateway_group_id,
+            allow_origination: record.allow_origination,
+            allow_termination: record.allow_termination
+          }
+        end
       end
     end
   end
--- spec/requests/api/rest/clickhouse_dictionaries/routing_plans_spec.rb
+++ spec/requests/api/rest/clickhouse_dictionaries/routing_plans_spec.rb
@@ -1,19 +1,19 @@
 # frozen_string_literal: true
 
-RSpec.describe '/api/rest/clickhouse-dictionaries/routing-plans' do
-  subject do
-    get clickhouse_dictionary_path
-  end
+RSpec.describe Api::Rest::ClickhouseDictionaries::RoutingPlansController do
+  include_context :clickhouse_dictionaries_api_helpers, type: :'routing-plans'
 
-  include_context :clickhouse_dictionaries_api_helpers do
-    let(:clickhouse_dictionary_type) { 'routing-plans' }
-  end
+  describe 'GET /api/rest/clickhouse-dictionaries/routing-plans' do
+    subject do
+      get clickhouse_dictionary_request_path
+    end
 
-  let!(:routing_plans) { create_list(:routing_plan, 3) }
+    let!(:routing_plans) { create_list(:routing_plan, 3) }
 
-  include_examples :responds_with_correct_json_each_row do
-    let(:expected_rows) do
-      routing_plans.map { |record| { id: record.id, name: record.name, external_id: record.external_id } }
+    include_examples :responds_with_correct_json_each_row do
+      let(:expected_rows) do
+        routing_plans.map { |record| { id: record.id, name: record.name, external_id: record.external_id } }
+      end
     end
   end
 end
