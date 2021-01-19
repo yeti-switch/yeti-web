ba7a578543085cf264caee77cdafa76a40623795
paratskiy <bog4242@gmail.com>
2021-01-19 11:33:27 +0200

    add tests to numberlists for pagination

--- spec/requests/api/rest/admin/routing/numberlists_spec.rb
+++ spec/requests/api/rest/admin/routing/numberlists_spec.rb
@@ -3,19 +3,19 @@
 RSpec.describe Api::Rest::Admin::Routing::NumberlistsController, type: :request do
   include_context :json_api_admin_helpers, type: :numberlists, prefix: '/api/rest/admin/routing'
 
-  describe 'GET /api/rest/admin/equipment/numberlists' do
+  describe 'GET /api/rest/admin/routing/numberlists' do
     subject do
       get json_api_request_path, params: nil, headers: json_api_request_headers
     end
 
-    let!(:number_lists) do
+    let!(:numberlists) do
       FactoryBot.create_list(:numberlist, 2)
     end
 
     include_examples :jsonapi_responds_with_pagination_links
     include_examples :returns_json_api_collection do
       let(:json_api_collection_ids) do
-        number_lists.map { |r| r.id.to_s }
+        numberlists.map { |r| r.id.to_s }
       end
     end
   end
