2cb6ce3740644992d383d5c76fea97c52ff83a93
paratskiy <bog4242@gmail.com>
2021-01-19 16:02:39 +0200

    rewrite clickhouse_dictionaries_api_helpers to receive type as parameter

--- spec/support/contexts/clickhouse_dictionaries_api_helpers.rb
+++ spec/support/contexts/clickhouse_dictionaries_api_helpers.rb
@@ -1,12 +1,9 @@
 # frozen_string_literal: true
 
-RSpec.shared_context :clickhouse_dictionaries_api_helpers do
-  let(:clickhouse_dictionary_path) do
-    "/api/rest/clickhouse-dictionaries/#{clickhouse_dictionary_type}"
-  end
-  let(:clickhouse_dictionary_type) do
-    raise "override let(:clickhouse_dictionary_type) in #{full_description.inspect} test"
-  end
+RSpec.shared_context :clickhouse_dictionaries_api_helpers do |type: nil|
+  let(:clickhouse_dictionary_resource_type) { type.to_s }
+  let(:clickhouse_dictionary_request_path_prefix) { '/api/rest/clickhouse-dictionaries' }
+  let(:clickhouse_dictionary_request_path) { "#{clickhouse_dictionary_request_path_prefix}/#{clickhouse_dictionary_resource_type}" }
 
   def response_json_each_row
     response.body.split("\n").map { |row| JSON.parse(row).deep_symbolize_keys }
