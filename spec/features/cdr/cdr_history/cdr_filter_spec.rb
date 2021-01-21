dbcc0302c5c041fb8f47fc2009991b0a0e46a61c
paratskiy <bog4242@gmail.com>
2021-01-21 14:44:44 +0200

    rewrite tests for cdr filter by tagged

--- spec/features/cdr/cdr_history/cdr_filter_spec.rb
+++ spec/features/cdr/cdr_history/cdr_filter_spec.rb
@@ -1,48 +1,56 @@
-RSpec.describe 'Cdrs index page', type: :feature, js: true do
+RSpec.describe 'Cdrs index page filtering', js: true do
   subject do
     within_filters { click_submit('Filter') }
   end
 
   include_context :login_as_admin
-  let!(:cdrs) { create_list(:cdr, 2) }
 
   before do
     Cdr::Cdr.delete_all
     visit cdrs_path
+
+    within_filters do
+      select_by_value tagged_filter_value, from: 'Tagged'
+    end
   end
 
-  describe 'filtering', js: true do
-    context 'with filter by Tagged' do
-      # before do
-      #
-      # end
+  let!(:cdrs) { create_list(:cdr, 2) }
+  let!(:tag) { create :routing_tag }
+  let!(:cdr_tagged) { create :cdr, routing_tag_ids: [tag.id] }
+  let(:tagged_filter_value) { nil }
 
-      let!(:tag) { create :routing_tag }
-      let!(:cdr_tagged) { create :cdr, routing_tag_ids: [tag.id] }
+  context 'with filter by tagged' do
+    let(:tagged_filter_value) { true }
 
-      it 'shows filtered records with routing_tags' do
-        within_filters do
-          select :Yes, from: :Tagged
-        end
+    it 'shows filtered records with routing_tags' do
+      subject
+      expect(page).to have_table
+      expect(page).to have_table_row count: 1
+      expect(page).to have_table_cell column: 'Id', text: cdr_tagged.id
+      expect(page).to have_select :Tagged, selected: 'Yes'
+    end
+  end
 
-        subject
-        expect(page).to have_table
-        expect(page).to have_table_row count: 1
-        expect(page).to have_table_cell column: 'Id', text: cdr_tagged.id
-        expect(page).to have_select :Tagged, selected: 'Yes'
-      end
+  context 'with filter by not tagged' do
+    let(:tagged_filter_value) { false }
 
-      it 'shows filtered records without routing_tags' do
-        within_filters do
-          select :No, from: :Tagged
-        end
+    it 'shows filtered records without routing_tags' do
+      subject
+      expect(page).to have_table
+      expect(page).to have_table_row count: 2
+      expect(page).to have_table_cell column: 'Id', text: cdrs.first.id
+      expect(page).to have_select :Tagged, selected: 'No'
+    end
+  end
 
-        subject
-        expect(page).to have_table
-        expect(page).to have_table_row count: 2
-        expect(page).to have_table_cell column: 'Id', text: cdrs.first.id
-        expect(page).to have_select :Tagged, selected: 'No'
-      end
+  context 'with filter by any tagged' do
+    it 'shows filtered records without routing_tags' do
+      subject
+      expect(page).to have_table
+      expect(page).to have_table_row count: Cdr::Cdr.count
+      expect(page).to have_table_cell column: 'Id', text: cdrs.first.id
+      expect(page).to have_table_cell column: 'Id', text: cdr_tagged.id
+      expect(page).to have_select :Tagged, selected: 'Any'
     end
   end
 end
