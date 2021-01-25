988f27ae361a5bacd960370ba480b6f2c4901aa4
Igor Gonchar <igor.gonchar@gmail.com>
2021-01-25 16:45:32 +0200

    app configuration, load defaults 5.2

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ Bundler.require(*Rails.groups)
 
 module Yeti
   class Application < Rails::Application
-    config.load_defaults '5.1'
+    config.load_defaults '5.2'
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
