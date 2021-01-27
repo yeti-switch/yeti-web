a63fcaf1dfee2eee6135ed54431423491fe4d4b0
paratskiy <bog4242@gmail.com>
2021-01-27 18:20:55 +0200

    fix bugs with testing

--- spec/features/realtime_data/sip_options_probers/index_sip_options_probers_spec.rb
+++ spec/features/realtime_data/sip_options_probers/index_sip_options_probers_spec.rb
@@ -5,23 +5,20 @@ RSpec.describe 'Sip Options Probers index page', js: true do
 
   subject do
     visit sip_options_probers_path
+  end
+
+  before do
     stub_jrpc_request('options_prober.show.probers', nodes.first.rpc_endpoint, { logger: be_present }).and_return([{ id: nodes.first.id }])
     stub_jrpc_request('options_prober.show.probers', nodes.second.rpc_endpoint, { logger: be_present }).and_return([{ id: nodes.second.id }])
   end
 
   let!(:nodes) { create_list(:node, 2) }
-  let(:jrpc_response) do
-    NodeRpcClient.perform_parallel(default: []) do |client, node|
-      result = client.sip_options_probers
-      result.map { |row| row.merge(node: node) }
-    end
-  end
 
   it 'returns correct Sip Options Probers' do
     subject
 
     expect(page).to have_table
-    expect(page).to have_table_row count: jrpc_response.size
-    jrpc_response.each { |sip_options_prober| expect(page).to have_table_cell column: 'Id', text: sip_options_prober[:id] }
+    expect(page).to have_table_row count: nodes.size
+    nodes.each { |node| expect(page).to have_table_cell column: 'Id', text: node[:id] }
   end
 end
