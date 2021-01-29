b72d98c1d78238bdd697453bd3f0d708f2c1e97d
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2021-01-29 02:22:17 +0200

    use secret variables for auth (#859)
    * use secret variables for auth

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -3,18 +3,15 @@
 set -e
 
 function usage(){
-	echo -e "usage: $0 api_base release distribution component deb_file1 [ deb_file2 ...]"
+	echo -e "usage: $0 release distribution component deb_file1 [ deb_file2 ...]"
 	exit 1
 }
 
-[ -z "$1" ] && echo -e "\nmissed api base\n" && usage
-[ -z "$2" ] && echo -e "\nmissed release\n" && usage
-[ -z "$3" ] && echo -e "\nmissed distribution\n" && usage
-[ -z "$4" ] && echo -e "\nmissed component\n" && usage
-[ -z "$5" ] && echo -e "\nmissed package\n" && usage
+[ -z "$1" ] && echo -e "\nmissed release\n" && usage
+[ -z "$2" ] && echo -e "\nmissed distribution\n" && usage
+[ -z "$3" ] && echo -e "\nmissed component\n" && usage
+[ -z "$4" ] && echo -e "\nmissed package\n" && usage
 
-api_base="$1"
-shift
 release="$1"
 shift
 distr="$1"
@@ -34,12 +31,12 @@ function fail {
 
 for file in $files; do
 	echo -e "\n\e[33m> upload pkg \e[39m $file\n"
-	curl -sSfF file=@"$file" "${api_base}/files/$dir" || fail "can't upload pkg file $file"
+	curl -sSfF file=@"$file" "${PKG_API_BASE}/files/$dir" || fail "can't upload pkg file $file"
 done
 
 echo -e "\n\e[33m> add pkg to repo $repo\e[39m\n"
-curl -sSf -X POST "${api_base}/repos/$repo/file/$dir" || fail "can't add pkg to repo"
+curl -sSf -X POST "${PKG_API_BASE}/repos/$repo/file/$dir" || fail "can't add pkg to repo"
 
 echo -e "\n\e[33m> update publish $release/$distr \e[39m\n"
-curl -sSf -X PUT -H 'Content-Type: application/json' --data '{ "Signing": {"PassphraseFile":"/home/pkg/gpg_pass"} }' "${api_base}/publish/${release}/${distr}" || fail "can't publish"
+curl -sSf -X PUT -H 'Content-Type: application/json' --data '{ "Signing": {"PassphraseFile":"/home/pkg/gpg_pass"} }' "${PKG_API_BASE}/publish/${release}/${distr}" || fail "can't publish"
 
