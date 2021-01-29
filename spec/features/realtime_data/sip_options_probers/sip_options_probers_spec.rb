6275f2fa7e947db5d71f6a225452692cdf03e488
paratskiy <bog4242@gmail.com>
2021-01-29 14:06:26 +0200

    update tests

--- spec/features/realtime_data/sip_options_probers/sip_options_probers_spec.rb
+++ spec/features/realtime_data/sip_options_probers/sip_options_probers_spec.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
-RSpec.describe 'Sip Options Probers index page', js: true do
+RSpec.describe 'Sip Options Probers', js: true do
   include_context :login_as_admin
-
-  subject do
-    visit sip_options_probers_path
-  end
 
   before do
     stub_jrpc_request('options_prober.show.probers', nodes.first.rpc_endpoint, { logger: be_present })
@@ -22,25 +18,36 @@ RSpec.describe 'Sip Options Probers index page', js: true do
     ]
   end
 
-  it 'returns correct Sip Options Probers' do
-    subject
-
-    expect(page).to have_table
-    expect(page).to have_table_row count: nodes.size
-    nodes.each { |node| expect(page).to have_link(node.name, href: node_path(node.id)) }
-    record_attributes.each do |record_attribute|
-      expect(page).to have_link(record_attribute[:name], href: equipment_sip_options_prober_path(record_attribute[:id]))
+  describe 'index page' do
+    subject do
+      visit sip_options_probers_path
     end
-    record_attributes.each { |record_attribute| expect(page).to have_table_cell column: 'Id', text: record_attribute[:id] }
+
+    it 'returns correct Sip Options Probers' do
+      subject
+
+      expect(page).to have_table
+      expect(page).to have_table_row count: nodes.size
+      nodes.each { |node| expect(page).to have_link(node.name, href: node_path(node.id)) }
+      record_attributes.each do |record_attribute|
+        expect(page).to have_link(record_attribute[:name], href: equipment_sip_options_prober_path(record_attribute[:id]))
+      end
+      record_attributes.each { |record_attribute| expect(page).to have_table_cell column: 'Id', text: record_attribute[:id] }
+    end
   end
 
-  it 'returns correct Sip Options Prober{#id}' do
-    subject
-
-    record_attributes.each do |record_attribute|
-      expect(page).to have_link(record_attribute[:id].to_s, href: sip_options_prober_path("#{record_attribute[:node_id]}*#{record_attribute[:id]}"))
+  describe 'show page' do
+    subject do
+      visit sip_options_probers_path
+      click_link(record_attributes.first[:id])
     end
-    click_link(record_attributes.first[:id])
-    record_attributes.first.each { |attribute| expect(page).to have_table_cell text: attribute }
+
+    it 'returns correct Sip Options Prober{#id}' do
+      subject
+
+      record_attributes.first.each do |attribute|
+        expect(page).to have_attribute_row(attribute.first.to_s.upcase, exact_text: attribute.second.to_s.upcase)
+      end
+    end
   end
 end
