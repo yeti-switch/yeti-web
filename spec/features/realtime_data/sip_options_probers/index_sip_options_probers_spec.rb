fb0117e3a485edb7ac6f8a4ce3dc6897551c81c3
paratskiy <bog4242@gmail.com>
2021-01-29 10:28:09 +0200

    add test for show sip options probers

--- spec/features/realtime_data/sip_options_probers/index_sip_options_probers_spec.rb
+++ spec/features/realtime_data/sip_options_probers/index_sip_options_probers_spec.rb
@@ -17,8 +17,8 @@ RSpec.describe 'Sip Options Probers index page', js: true do
   let!(:nodes) { create_list(:node, 2) }
   let(:record_attributes) do
     [
-      FactoryBot.attributes_for(:sip_options_prober, :filled),
-      FactoryBot.attributes_for(:sip_options_prober, :filled)
+      FactoryBot.attributes_for(:sip_options_prober, :filled, node_id: nodes.first.id),
+      FactoryBot.attributes_for(:sip_options_prober, :filled, node_id: nodes.second.id)
     ]
   end
 
@@ -32,5 +32,16 @@ RSpec.describe 'Sip Options Probers index page', js: true do
       expect(page).to have_link(record_attribute[:name], href: equipment_sip_options_prober_path(record_attribute[:id]))
     end
     record_attributes.each { |record_attribute| expect(page).to have_table_cell column: 'Id', text: record_attribute[:id] }
+  end
+
+  it 'returns correct Sip Options Prober{#id}' do
+    subject
+
+    record_attributes.each do |record_attribute|
+      expect(page).to have_link(record_attribute[:id].to_s, href: sip_options_prober_path("#{record_attribute[:node_id]}*#{record_attribute[:id]}"))
+    end
+    click_link({ href: sip_options_prober_path("#{record_attributes.first[:node_id]}*#{record_attributes.first[:id]}")})
+    click_link(record_attributes.first[:id])
+    record_attributes.first.each { |attribute| expect(page).to have_table_cell text: attribute }
   end
 end
