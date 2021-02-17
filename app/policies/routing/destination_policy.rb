fd63e07060c06ab8fb5d2eda665909f246af6dd0
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2021-02-17 17:32:24 +0200

    Add policy for :truncate_long_time_stats? action. closes #878 (#888)

--- app/policies/routing/destination_policy.rb
+++ app/policies/routing/destination_policy.rb
@@ -6,6 +6,7 @@ module Routing
 
     alias_rule :clear_quality_alarm?, to: :perform?
     alias_rule :truncate_short_window_stats?, to: :perform? # DSL acts_as_quality_stat
+    alias_rule :truncate_long_time_stats?, to: :perform? # DSL acts_as_stat
     alias_rule :enable?, :disable?, to: :perform? # DSL acts_as_status
 
     class Scope < ::RolePolicy::Scope
