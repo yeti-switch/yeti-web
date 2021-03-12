7ef93e6a2dfc3704937ceab70b76b104b9f23559
Denis Talakevich <senid231@gmail.com>
2021-03-12 12:09:50 +0200

    fix gateways show page when node is down
    regression were added with NodeApi refactoring

--- spec/features/equipment/gateways/index_gateways_spec.rb
+++ spec/features/equipment/gateways/index_gateways_spec.rb
@@ -1,17 +1,25 @@
 # frozen_string_literal: true
 
 RSpec.describe 'Index Gateways', type: :feature do
+  subject do
+    visit gateways_path
+  end
+
   include_context :login_as_admin
 
-  let(:vendor) { create(:vendor) }
-  let(:gateway_group) { create(:gateway_group, vendor: vendor) }
-  it 'n+1 checks' do
-    gateways = create_list(
-      :gateway, 2, :filled,
+  let!(:vendor) { create(:vendor) }
+  let!(:gateway_group) { create(:gateway_group, vendor: vendor) }
+  let!(:gateways) do
+    create_list(
+      :gateway, 2,
+      :filled,
       contractor: vendor,
       gateway_group: gateway_group
     )
-    visit gateways_path
+  end
+
+  it 'n+1 checks' do
+    subject
     gateways.each do |gateway|
       expect(page).to have_css('.resource_id_link', text: gateway.id)
     end
