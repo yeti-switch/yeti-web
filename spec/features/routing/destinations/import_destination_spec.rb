4007d207a8c1cb2c8b471044452c615a7859418b
Artem Lutsenko <artemlutsenko@admins-MacBook-Pro.local>
2021-04-15 14:58:37 +0300

    fix DestinationRatePolicy namespace and add test for /destination_imports

---
+++ spec/features/routing/destinations/import_destination_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe 'Destination imports', type: :feature do
+  include_context :login_as_admin
+
+  subject { visit destination_imports_path }
+
+  context 'with importing destination items' do
+    let!(:importing_destination) { create(:importing_destination) }
+
+    it 'should have table with items' do
+      subject
+      expect(page).to have_table
+      expect(page).to have_table_row(id: importing_destination.id)
+    end
+  end
+
+  context 'without importing destintaion items' do
+    it 'shouldn`t have table with items' do
+      subject
+      expect(page).to_not have_table
+      expect(page).to have_text('There are no Destination Imports yet.')
+    end
+  end
+end
