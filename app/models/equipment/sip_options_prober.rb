40eeae28959b3a3bf2ac96b8c986dc1c47bbcd9e
Artem Lutsenko <artem.luceno@gmail.com>
2021-04-20 18:41:27 +0300

    add unique index to external_id colund and validation in models

--- app/models/equipment/sip_options_prober.rb
+++ app/models/equipment/sip_options_prober.rb
@@ -48,7 +48,7 @@ class Equipment::SipOptionsProber < Yeti::ActiveRecord
   belongs_to :node, optional: true
   belongs_to :sip_schema, class_name: 'System::SipSchema', foreign_key: :sip_schema_id
 
-  validates :name, uniqueness: { allow_blank: false }
+  validates :name, :external_id, uniqueness: { allow_blank: false }
   validates :name, :ruri_domain, :ruri_username, :transport_protocol, :proxy_transport_protocol, :sip_schema, presence: true
 
   # validates_format_of :contact, :with => /\Asip:(.*)\z/
--- db/migrate/20210415123322_add_external_id_to_equipment_sip_options_probers.rb
+++ db/migrate/20210415123322_add_external_id_to_equipment_sip_options_probers.rb
@@ -1,5 +1,6 @@
 class AddExternalIdToEquipmentSipOptionsProbers < ActiveRecord::Migration[5.2]
   def change
     add_column 'class4.sip_options_probers', :external_id, :bigint
+    add_index 'class4.sip_options_probers', :external_id, unique: true
   end
 end
