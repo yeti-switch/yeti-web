9ff92474ee9e1c86351cc61a2234ffbc889eaa45
Artem Lutsenko <artem.luceno@gmail.com>
2021-04-27 17:43:15 +0300

    replace attr to prop

--- app/assets/javascripts/build_tags.js
+++ app/assets/javascripts/build_tags.js
@@ -1,13 +1,19 @@
 // Build chosen field for routing tag ids select on update batch action form
 
 $(document).on('mass_update_modal_dialog:after_open', function (event, form) {
-    var tagsChosen = $(form).find("select[name='routing_tag_ids']").attr({'name': 'routing_tag_ids[]', 'class': 'chosen', 'id': 'routing_tag_ids'});
+    var tagsChosen = $(form).find("select[name='routing_tag_ids']").prop({
+        'name': 'routing_tag_ids[]', 
+        'class': 'chosen', 
+        'id': 'routing_tag_ids',
+        'value': '', // reset default value
+        'multiple': true
+    });
 
     if (tagsChosen.length === 0) {
         return;
     }
 
-    var hidden = $('<input>').attr({
+    var hidden = $('<input>').prop({
         'type': 'hidden',
         'id': 'routing_tag_ids',
         'name': 'routing_tag_ids',
@@ -15,16 +21,13 @@ $(document).on('mass_update_modal_dialog:after_open', function (event, form) {
         'disabled': true
     }).appendTo(form);
 
-    tagsChosen.prop('value', ''); // reset default value
-    tagsChosen.prop('multiple', true);
-
     tagsChosen.chosen({no_results_text: "No results matched", width: '240px', search_contains: true, allow_single_deselect: true});
 
     tagsChosen.change({hidden: hidden}, function(event){
         if ($(event.target).val() != ''){
-            event.data.hidden.attr('disabled', true);
+            event.data.hidden.prop('disabled', true);
         }else{
-            event.data.hidden.attr('disabled', false);
+            event.data.hidden.prop('disabled', false);
         }
     });
 });
@@ -33,7 +36,8 @@ $(document).on('chosen:updated', function(event){
     if ($(event.target).prop('id') != 'routing_tag_ids') { return; }
     
     var tagChosen = $('select#routing_tag_ids.chosen');
-    var disabled = tagChosen.attr('disabled') || false
+    var disabled = tagChosen.prop('disabled')
+    console.log(disabled);
 
     if (disabled) { 
         $("[name='routing_tag_ids']").prop('disabled', true); 
