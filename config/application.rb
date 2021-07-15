3df50c48df3a7b0c606d6ef34f9fd7c72e1c0a3e
Denis Talakevich <senid231@gmail.com>
2021-07-15 18:21:34 +0300

    apply rails 6.1 config defaults

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ Bundler.require(*Rails.groups)
 module Yeti
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.0
+    config.load_defaults 6.1
 
     # changing defaults
     Rails.application.config.action_view.default_enforce_utf8 = true
