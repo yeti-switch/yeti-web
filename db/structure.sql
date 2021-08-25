ce41bfe4899082064c9d560e9b446a181c65f4ed
Dmitry Sinina <dmitry.sinina@onat.edu.ua>
2021-08-25 22:30:32 +0300

    switch20: fix routing tags detection

--- db/structure.sql
+++ db/structure.sql
@@ -18034,8 +18034,8 @@ CREATE FUNCTION switch20.route(i_node_id integer, i_pop_id integer, i_protocol_i
           yeti_ext.tag_compare(routing_tag_ids, v_call_tags, routing_tag_mode_id ) > 0
         order by
           yeti_ext.tag_compare(routing_tag_ids, v_call_tags, routing_tag_mode_id) desc,
-          length(prefix_range(v_ret.src_prefix_routing)) desc,
-          length(prefix_range(v_ret.dst_prefix_routing)) desc,
+          length(prefix_range(src_prefix)) desc,
+          length(prefix_range(dst_prefix)) desc,
           src_area_id is null,
           dst_area_id is null
         limit 1;
@@ -19205,8 +19205,8 @@ CREATE FUNCTION switch20.route_debug(i_node_id integer, i_pop_id integer, i_prot
           yeti_ext.tag_compare(routing_tag_ids, v_call_tags, routing_tag_mode_id ) > 0
         order by
           yeti_ext.tag_compare(routing_tag_ids, v_call_tags, routing_tag_mode_id) desc,
-          length(prefix_range(v_ret.src_prefix_routing)) desc,
-          length(prefix_range(v_ret.dst_prefix_routing)) desc,
+          length(prefix_range(src_prefix)) desc,
+          length(prefix_range(dst_prefix)) desc,
           src_area_id is null,
           dst_area_id is null
         limit 1;
@@ -20312,8 +20312,8 @@ CREATE FUNCTION switch20.route_release(i_node_id integer, i_pop_id integer, i_pr
           yeti_ext.tag_compare(routing_tag_ids, v_call_tags, routing_tag_mode_id ) > 0
         order by
           yeti_ext.tag_compare(routing_tag_ids, v_call_tags, routing_tag_mode_id) desc,
-          length(prefix_range(v_ret.src_prefix_routing)) desc,
-          length(prefix_range(v_ret.dst_prefix_routing)) desc,
+          length(prefix_range(src_prefix)) desc,
+          length(prefix_range(dst_prefix)) desc,
           src_area_id is null,
           dst_area_id is null
         limit 1;
