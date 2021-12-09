ea8a2e5afaaa9fee4d1ce65db05edb81764c1668
Kirill Shestakov <freyr.sh@gmail.com>
2021-12-09 18:35:25 +0200

    fix package version generation for release branch

--- ci/gen_version.sh
+++ ci/gen_version.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 if git describe --tags --exact &>/dev/null; then
-	git describe --tags --exact | sed 's/$/+nightly0/'
+	if [ "$PACKAGE_TYPE" = "nightly" ]; then
+		git describe --tags --exact | sed 's/$/+nightly0/'
+	else
+		git describe --tags --exact
+	fi
 else
 	git describe --tags | sed -E 's/-([0-9]+)-[^-]*$/+nightly\1/'
 fi
