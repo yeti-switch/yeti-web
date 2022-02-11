044710d9ccc74f49f62dfb5fd19a7a31e3225164
Artem Lutsenko <artem.l@didww.com>
2022-02-11 12:06:26 +0200

    draft commit fix contractor filter

--- lib/resource_dsl/contractor_filter.rb
+++ lib/resource_dsl/contractor_filter.rb
@@ -16,7 +16,7 @@ module ResourceDSL
         },
         collection: proc {
           resource_id = params.fetch(:q, {})[name]
-          resource_id ? Contractor.ransack(q).result.where(id: resource_id) : []
+          resource_id ? Contractor.ransack(q[:q]).result.where(id: resource_id) : []
         }
       }
       filter_options = options.deep_merge(filter_options)
