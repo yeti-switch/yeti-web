981149a21ee28d2dbaefa61dbfa38e9216419eeb
Denis Talakevich <senid231@gmail.com>
2022-02-21 21:54:13 +0200

    fix structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -29971,7 +29971,8 @@ ALTER TABLE ONLY sys.sensors
 -- PostgreSQL database dump complete
 --
 
-SET search_path TO gui, public, switch, billing, class4, runtime_stats, sys, logs, data_import;
+SET search_path TO gui, public, switch, billing, class4, runtime_stats, sys, logs, data_import
+;
 
 INSERT INTO "public"."schema_migrations" (version) VALUES
 ('20170822151410'),
