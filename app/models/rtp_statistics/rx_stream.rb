3dc497121facf6fcb998ddd0c148113352c9a71e
Dmitry <dmitry.sinina@onat.edu.ua>
2022-04-28 14:54:29 +0300

    update annotations

--- app/models/rtp_statistics/rx_stream.rb
+++ app/models/rtp_statistics/rx_stream.rb
@@ -1,5 +1,42 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: rtp_statistics.rx_streams
+#
+#  id                     :bigint(8)        not null, primary key
+#  local_host             :inet
+#  local_port             :integer(4)
+#  local_tag              :string
+#  remote_host            :inet
+#  remote_port            :integer(4)
+#  rx_bytes               :integer(4)
+#  rx_decode_errors       :integer(4)
+#  rx_packet_delta_max    :float
+#  rx_packet_delta_mean   :float
+#  rx_packet_delta_min    :float
+#  rx_packet_delta_std    :float
+#  rx_packet_jitter_max   :float
+#  rx_packet_jitter_mean  :float
+#  rx_packet_jitter_min   :float
+#  rx_packet_jitter_std   :float
+#  rx_packets             :integer(4)
+#  rx_payloads_relayed    :string           is an Array
+#  rx_payloads_transcoded :string           is an Array
+#  rx_rtcp_jitter_max     :float
+#  rx_rtcp_jitter_mean    :float
+#  rx_rtcp_jitter_min     :float
+#  rx_rtcp_jitter_std     :float
+#  rx_ssrc                :integer(4)
+#  rx_total_lost          :integer(4)
+#  time_end               :datetime
+#  time_start             :datetime         not null
+#  gateway_external_id    :bigint(8)
+#  gateway_id             :bigint(8)
+#  node_id                :integer(4)
+#  pop_id                 :integer(4)
+#  tx_stream_id           :bigint(8)
+#
 class RtpStatistics::RxStream < Cdr::Base
   self.table_name = 'rtp_statistics.rx_streams'
   self.primary_key = :id
--- app/models/rtp_statistics/tx_stream.rb
+++ app/models/rtp_statistics/tx_stream.rb
@@ -1,5 +1,37 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: rtp_statistics.tx_streams
+#
+#  id                      :bigint(8)        not null, primary key
+#  local_host              :inet
+#  local_port              :integer(4)
+#  local_tag               :string           not null
+#  rtcp_rtt_max            :float
+#  rtcp_rtt_mean           :float
+#  rtcp_rtt_min            :float
+#  rtcp_rtt_std            :float
+#  rx_dropped_packets      :integer(4)
+#  rx_out_of_buffer_errors :integer(4)
+#  rx_rtp_parse_errors     :integer(4)
+#  time_end                :datetime
+#  time_start              :datetime         not null
+#  tx_bytes                :integer(4)
+#  tx_packets              :integer(4)
+#  tx_payloads_relayed     :string           is an Array
+#  tx_payloads_transcoded  :string           is an Array
+#  tx_rtcp_jitter_max      :float
+#  tx_rtcp_jitter_mean     :float
+#  tx_rtcp_jitter_min      :float
+#  tx_rtcp_jitter_std      :float
+#  tx_ssrc                 :integer(4)
+#  tx_total_lost           :integer(4)
+#  gateway_external_id     :bigint(8)
+#  gateway_id              :bigint(8)
+#  node_id                 :integer(4)
+#  pop_id                  :integer(4)
+#
 class RtpStatistics::TxStream < Cdr::Base
   self.table_name = 'rtp_statistics.tx_streams'
   self.primary_key = :id
