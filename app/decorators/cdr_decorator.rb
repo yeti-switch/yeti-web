4e35afc167e6ff3f9fd706bc299da1155a9a24cc
Dmitry Sinina <dmitry.s@yeti-switch.org>
2022-10-27 12:43:36 +0300

    CDR decorations (#1187)
    * CDR decorations
    * make rubocop happy

--- app/decorators/cdr_decorator.rb
+++ app/decorators/cdr_decorator.rb
@@ -4,6 +4,18 @@ class CdrDecorator < Draper::Decorator
   delegate_all
   decorates Cdr::Cdr
 
+  def decorated_routing_delay
+    round5_format :routing_delay
+  end
+
+  def decorated_rtt
+    round5_format :rtt
+  end
+
+  def decorated_pdd
+    round5_format :pdd
+  end
+
   def routing_tags
     return nil if model.routing_tag_ids.blank?
 
@@ -15,5 +27,11 @@ class CdrDecorator < Draper::Decorator
         h.content_tag(:span, id, class: 'status_tag no')
       end
     end.join('&nbsp;').html_safe
+  end
+
+  def round5_format(attr)
+    return nil if model.public_send(attr).nil?
+
+    model.public_send(attr).round(5)
   end
 end
