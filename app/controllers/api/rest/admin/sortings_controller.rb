01e97bd52dbee4268063bb4ce8fc7932e6a195a2
Dmitry <dmitry.sinina@onat.edu.ua>
2022-12-31 21:19:18 +0200

    remove api resources

--- app/controllers/api/rest/admin/sortings_controller.rb
+++
@@ -1,4 +0,0 @@
-# frozen_string_literal: true
-
-class Api::Rest::Admin::SortingsController < Api::Rest::Admin::BaseController
-end
--- app/resources/api/rest/admin/sorting_resource.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Api::Rest::Admin::SortingResource < ::BaseResource
-  attributes :name, :description, :use_static_routes
-  paginator :paged
-  filter :name # DEPRECATED
-
-  ransack_filter :name, type: :string
-end
