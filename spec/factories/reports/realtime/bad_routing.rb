172f313e9ad26a2f9a218e86f1d4286849d9478f
Dmitry <dmitry.sinina@onat.edu.ua>
2023-01-02 22:45:42 +0200

    remove disconnect initiator table

--- spec/factories/reports/realtime/bad_routing.rb
+++ spec/factories/reports/realtime/bad_routing.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :bad_routing, class: Report::Realtime::BadRouting, parent: :cdr do
     time_start { 110.seconds.ago } # this record will be available in page during 10 second
-    disconnect_initiator { DisconnectInitiator.find_by(id: 0) || create(:disconnect_initiator, :traffic_manager) }
+    disconnect_initiator_id { Cdr::Cdr::DISCONNECT_INITIATOR_ROUTING }
     association :customer_auth, factory: :customers_auth
   end
 end
