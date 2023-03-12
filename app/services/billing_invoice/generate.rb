553d35dcc3bfaecc730e88f787e5453714e0e19a
Denis Talakevich <senid231@gmail.com>
2023-03-12 22:31:59 +0200

    shift generated invoice start date when has invoice that partly covers this period

--- app/services/billing_invoice/generate.rb
+++ app/services/billing_invoice/generate.rb
@@ -13,11 +13,12 @@ module BillingInvoice
           is_vendor: is_vendor,
           period_end: account_invoice_at
         )
+        start_time = choose_start_time! invoice_params[:start_time]
 
         invoice = BillingInvoice::Create.call(
           account: account,
           is_vendor: is_vendor,
-          start_time: invoice_params[:start_time],
+          start_time: start_time,
           end_time: account_invoice_at,
           type_id: account_invoice_type_id
         )
@@ -33,6 +34,23 @@ module BillingInvoice
     end
 
     private
+
+    # When there is invoice that have end_date newer (bigger) than calculated start_time
+    # we move invoice start_date to this end_date,
+    # because otherwise creation will be failed.
+    # @param calc_start_time [Time] calculated start_date for new invoice based on account.
+    # @return [Time] start_date that should be used for new invoice based on currently existing invoices.
+    def choose_start_time!(calc_start_time)
+      invoice_scope = Billing::Invoice.where(account_id: account.id, vendor_invoice: is_vendor)
+      last_invoice_end_date = invoice_scope.pick(Arel.sql('MAX(end_date)'))
+      return calc_start_time if last_invoice_end_date.nil?
+
+      if last_invoice_end_date >= account_invoice_at
+        raise Error, 'invoice exists with end_date greater than or equal to current end_date'
+      end
+
+      [calc_start_time, last_invoice_end_date].max
+    end
 
     def schedule_next_invoice!(invoice_params)
       if is_vendor
