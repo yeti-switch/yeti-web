98b3ab0477418800bb6670f0ec0f36d37c596aac
Denis Talakevich <senid231@gmail.com>
2023-04-27 13:07:18 +0300

    add RAILS_LOG_TO_STDOUT to test env

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -70,4 +70,10 @@ Rails.application.configure do
     Bullet.bullet_logger = true
     Bullet.raise = true
   end
+
+  if ENV['RAILS_LOG_TO_STDOUT'].present?
+    logger = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger = ActiveSupport::TaggedLogging.new(logger)
+  end
 end
