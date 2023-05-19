321ea5399c6dc05465354bdfc3806d6abe68d93a
Dmitry <dmitry.sinina@onat.edu.ua>
2023-05-19 16:22:54 +0300

    add pgq queue creation

--- db/seeds/cdr/pgq.sql
+++ db/seeds/cdr/pgq.sql
@@ -4,3 +4,6 @@ select pgq.create_queue('cdr_streaming');
 select pgq.create_queue('rtp_statistics');
 select pgq.create_queue('rtp_rx_stream');
 select pgq.create_queue('rtp_tx_stream');
+
+select pgq.create_queue('async_cdr_statistics');
+select pgq.register_consumer('async_cdr_statistics', 'async_cdr_statistics');
