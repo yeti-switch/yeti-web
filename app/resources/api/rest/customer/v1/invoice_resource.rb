56f44d52924257921e67c3754db5c87aaf1718cb
Denis Talakevich <senid231@gmail.com>
2023-12-20 09:55:37 +0200

    cleanup customer api invoices resource

--- app/resources/api/rest/customer/v1/invoice_resource.rb
+++ app/resources/api/rest/customer/v1/invoice_resource.rb
@@ -8,21 +8,21 @@ class Api::Rest::Customer::V1::InvoiceResource < Api::Rest::Customer::V1::BaseRe
              :end_date,
              :has_pdf
 
-  attribute :amount_spent, delegate: :amount_spent
+  attribute :amount_spent
 
-  attribute :originated_amount_spent, delegate: :originated_amount_spent
-  attribute :originated_calls_count, delegate: :originated_calls_count
-  attribute :originated_successful_calls_count, delegate: :originated_successful_calls_count
-  attribute :originated_calls_duration, delegate: :originated_calls_duration
-  attribute :originated_billing_duration, delegate: :originated_billing_duration
+  attribute :originated_amount_spent
+  attribute :originated_calls_count
+  attribute :originated_successful_calls_count
+  attribute :originated_calls_duration
+  attribute :originated_billing_duration
   attribute :originated_first_call_at, delegate: :first_originated_call_at
   attribute :originated_last_call_at, delegate: :last_originated_call_at
 
-  attribute :terminated_amount_spent, delegate: :terminated_amount_spent
-  attribute :terminated_calls_count, delegate: :terminated_calls_count
-  attribute :terminated_successful_calls_count, delegate: :terminated_successful_calls_count
-  attribute :terminated_calls_duration, delegate: :terminated_calls_duration
-  attribute :terminated_billing_duration, delegate: :terminated_billing_duration
+  attribute :terminated_amount_spent
+  attribute :terminated_calls_count
+  attribute :terminated_successful_calls_count
+  attribute :terminated_calls_duration
+  attribute :terminated_billing_duration
   attribute :terminated_first_call_at, delegate: :first_terminated_call_at
   attribute :terminated_last_call_at, delegate: :last_terminated_call_at
 
@@ -31,21 +31,21 @@ class Api::Rest::Customer::V1::InvoiceResource < Api::Rest::Customer::V1::BaseRe
   ransack_filter :reference, type: :string
   ransack_filter :start_date, type: :datetime
   ransack_filter :end_date, type: :datetime
-  ransack_filter :amount_spent, type: :number, column: :amount_spent
+  ransack_filter :amount_spent, type: :number
 
-  ransack_filter :originated_amount_spent, type: :number, column: :originated_amount_spent
-  ransack_filter :originated_calls_count, type: :number, column: :originated_calls_count
-  ransack_filter :originated_successful_calls_count, type: :number, column: :originated_successful_calls_count
-  ransack_filter :originated_calls_duration, type: :number, column: :originated_calls_duration
-  ransack_filter :originated_billing_duration, type: :number, column: :originated_billing_duration
+  ransack_filter :originated_amount_spent, type: :number
+  ransack_filter :originated_calls_count, type: :number
+  ransack_filter :originated_successful_calls_count, type: :number
+  ransack_filter :originated_calls_duration, type: :number
+  ransack_filter :originated_billing_duration, type: :number
   ransack_filter :originated_first_call_at, type: :datetime, column: :first_originated_call_at
   ransack_filter :originated_last_call_at, type: :datetime, column: :last_originated_call_at
 
-  ransack_filter :terminated_amount_spent, type: :number, column: :terminated_amount_spent
-  ransack_filter :terminated_calls_count, type: :number, column: :terminated_calls_count
-  ransack_filter :terminated_successful_calls_count, type: :number, column: :terminated_successful_calls_count
-  ransack_filter :terminated_calls_duration, type: :number, column: :terminated_calls_duration
-  ransack_filter :terminated_billing_duration, type: :number, column: :terminated_billing_duration
+  ransack_filter :terminated_amount_spent, type: :number
+  ransack_filter :terminated_calls_count, type: :number
+  ransack_filter :terminated_successful_calls_count, type: :number
+  ransack_filter :terminated_calls_duration, type: :number
+  ransack_filter :terminated_billing_duration, type: :number
   ransack_filter :terminated_first_call_at, type: :datetime, column: :first_terminated_call_at
   ransack_filter :terminated_last_call_at, type: :datetime, column: :last_terminated_call_at
 
