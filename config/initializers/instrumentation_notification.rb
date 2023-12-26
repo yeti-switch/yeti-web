9b1f6539b8342e5fa91a44c416794d756b275606
Ivanov-Anton <anton.i@didww.com>
2023-12-26 15:47:30 +0200

    YETIWEB-1396, Async writing to api log

--- config/initializers/instrumentation_notification.rb
+++ config/initializers/instrumentation_notification.rb
@@ -44,31 +44,36 @@ end
 ActiveSupport::Notifications.subscribe 'process_action.action_controller' do |_name, start, finish, _id, payload|
   if payload[:path].start_with? '/api/'
 
-    Log::ApiLog.create do |api_request|
-      debug_mode = payload[:debug_mode]
+    Thread.new do
+      Log::ApiLog.create do |api_request|
+        debug_mode = payload[:debug_mode]
 
-      api_request.meta = payload[:meta]
-      api_request.remote_ip = payload[:remote_ip]
+        api_request.meta = payload[:meta]
+        api_request.remote_ip = payload[:remote_ip]
 
-      if payload[:status].nil? && payload[:exception].present?
-        payload[:status] = ActionDispatch::ExceptionWrapper.status_code_for_exception(payload[:exception].class)
+        if payload[:status].nil? && payload[:exception].present?
+          payload[:status] = ActionDispatch::ExceptionWrapper.status_code_for_exception(payload[:exception].class)
+        end
+
+        api_request.path = payload[:path].to_s.split('?').first
+        api_request.page_duration = (finish - start) * 1000
+        api_request.db_duration = payload[:db_runtime]
+        api_request.method = payload[:method]
+        api_request.status = payload[:status]
+        api_request.controller = payload[:controller]
+        api_request.action = payload[:action]
+        api_request.params = payload[:params].to_yaml
+        if debug_mode
+          request_headers = payload[:request].headers.select { |key, _val| key.match('^HTTP.*') }
+          api_request.request_headers = Hash[*request_headers.flatten(1)].map { |k, v| "#{k}=#{v}" }.join("\n\r")
+          api_request.request_body = payload[:request].body.read
+          api_request.response_body = payload[:response].try(:body)
+          api_request.response_headers = (payload[:response].try(:headers) || {}).map { |k, v| "#{k}=#{v}" }.join("\n\r")
+        end
       end
-
-      api_request.path = payload[:path].to_s.split('?').first
-      api_request.page_duration = (finish - start) * 1000
-      api_request.db_duration = payload[:db_runtime]
-      api_request.method = payload[:method]
-      api_request.status = payload[:status]
-      api_request.controller = payload[:controller]
-      api_request.action = payload[:action]
-      api_request.params = payload[:params].to_yaml
-      if debug_mode
-        request_headers = payload[:request].headers.select { |key, _val| key.match('^HTTP.*') }
-        api_request.request_headers = Hash[*request_headers.flatten(1)].map { |k, v| "#{k}=#{v}" }.join("\n\r")
-        api_request.request_body = payload[:request].body.read
-        api_request.response_body = payload[:response].try(:body)
-        api_request.response_headers = (payload[:response].try(:headers) || {}).map { |k, v| "#{k}=#{v}" }.join("\n\r")
-      end
+    rescue StandardError => e
+      logger.error { "#{e.message}\n#{e.backtrace&.join("\n")}" }
+      CaptureError.capture(e, tags: { component: 'API Logs' })
     end
   end
 end
