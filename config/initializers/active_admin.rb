5d5718771b36c686ee8dffac1e3c1a086e4f069e
Ivanov-Anton <anton.i@didww.com>
2024-02-08 13:10:47 +0200

    YETIWEB-1410, specify string as default filter type when ransack predicate is equals, contains, starts_with and ends_with

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -230,4 +230,29 @@ ActiveAdmin.before_load do
   ActiveAdmin::ResourceDSL.include ApplicationHelper
 
   ActiveAdmin::ResourceController.prepend ActiveAdmin::CsvStreamWithErrorHandler
+
+  ActiveAdmin::Filters::FormBuilder.class_eval do
+    # Returns the default filter type for a given attribute. If you want
+    # to use a custom search method, you have to specify the type yourself.
+    def default_input_type(method, _options = {})
+      if method.match?(/_(eq|equals|cont|contains|start|starts_with|end|ends_with)\z/)
+        :string
+      elsif klass._ransackers.key?(method.to_s)
+        klass._ransackers[method.to_s].type
+      elsif reflection_for(method) || polymorphic_foreign_type?(method)
+        :select
+      elsif (column = column_for(method))
+        case column.type
+        when :date, :datetime
+          :date_range
+        when :string, :text
+          :string
+        when :integer, :float, :decimal
+          :numeric
+        when :boolean
+          :boolean
+        end
+      end
+    end
+  end
 end
