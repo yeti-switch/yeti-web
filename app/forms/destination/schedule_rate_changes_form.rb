748d3818f5265585185b888028bc85cedae4d08e
artemlutsenko <artem.l@didww.com>
2024-05-16 11:39:21 +0300

    schedule destinations rate changes improvement
    add option to create next rates with specifiс rate fields

--- app/forms/destination/schedule_rate_changes_form.rb
+++ app/forms/destination/schedule_rate_changes_form.rb
@@ -10,10 +10,11 @@ module Destination
     attribute :connect_fee
     attribute :ids_sql, :string
 
-    validates :initial_rate, :next_rate, :connect_fee, numericality: true
-    validates :initial_interval, :next_interval, numericality: { greater_than: 0, less_than_or_equal_to: ApplicationRecord::PG_MAX_SMALLINT }
+    validates :initial_rate, :next_rate, :connect_fee, numericality: true, allow_nil: true
+    validates :initial_interval, :next_interval, numericality: { greater_than: 0, less_than_or_equal_to: ApplicationRecord::PG_MAX_SMALLINT }, allow_nil: true
     validates :ids_sql, :apply_time, presence: true
     validate :validate_apply_time
+    validate :validate_rate_fields
 
     def self.form_inputs
       {
@@ -44,6 +45,12 @@ module Destination
       return if apply_time.nil?
 
       errors.add(:apply_time, 'must be in the future') unless apply_time.future?
+    end
+
+    def validate_rate_fields
+      if initial_interval.nil? && initial_rate.nil? && next_interval.nil? && next_rate.nil? && connect_fee.nil?
+        errors.add(:base, 'At least one of the following fields must be filled: initial_interval, initial_rate, next_interval, next_rate, connect_fee')
+      end
     end
   end
 end
