f8ece2d4c2b56b269bbe9fe12d8fb2df31ace24b
Dmitry <dmitry.sinina@onat.edu.ua>
2024-12-03 14:19:55 +0200

    add filters to transactions and services. refs #1633,  #1632

--- app/admin/billing/services.rb
+++ app/admin/billing/services.rb
@@ -45,7 +45,7 @@ ActiveAdmin.register Billing::Service, as: 'Services' do
   includes :type, :account
 
   filter :id
-  filter :created_at
+  filter :created_at, as: :date_time_range
   filter :name
   account_filter :account_id_eq
   filter :type_id,
@@ -56,9 +56,11 @@ ActiveAdmin.register Billing::Service, as: 'Services' do
          as: :select,
          input_html: { class: 'chosen' },
          collection: proc { Billing::Service::RENEW_PERIODS.invert }
+  filter :state_id, label: 'State', as: :select, collection: Billing::Service::STATES.invert
   filter :initial_price
   filter :renew_price
-  filter :renew_at
+  filter :renew_at, as: :date_time_range
+  filter :uuid_equals, label: 'UUID'
 
   scope :all
   scope :ready_for_renew
