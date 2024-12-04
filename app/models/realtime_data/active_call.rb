177ade89a8e9bc765936c7feff1718b3ccfae557
Dmitry <dmitry.sinina@onat.edu.ua>
2024-12-04 19:52:31 +0200

    add associations

--- app/models/realtime_data/active_call.rb
+++ app/models/realtime_data/active_call.rb
@@ -81,7 +81,10 @@ class RealtimeData::ActiveCall < YetiResource
   has_one :orig_gw, class_name: 'Gateway', foreign_key: :orig_gw_id
   has_one :term_gw, class_name: 'Gateway', foreign_key: :term_gw_id
   has_one :routing_group, class_name: 'Routing::RoutingGroup', foreign_key: :routing_group_id
+  has_one :routing_plan, class_name: 'Routing::RoutingPlan', foreign_key: :routing_plan_id
   has_one :rateplan, class_name: 'Routing::Rateplan', foreign_key: :rateplan_id
+  has_one :src_country, class_name: 'System::Country', foreign_key: :src_country_id
+  has_one :src_network, class_name: 'System::Network', foreign_key: :src_network_id
   has_one :dst_country, class_name: 'System::Country', foreign_key: :dst_country_id
   has_one :dst_network, class_name: 'System::Network', foreign_key: :dst_network_id
   has_one :node, class_name: 'Node', foreign_key: :node_id
