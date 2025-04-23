03ac2044b5c66d59cb162ba2f88be7c3093d8bfc
sdi <dmitry.s@yeti-switch.org>
2025-04-23 21:37:51 +0300

    fix tests

--- spec/support/examples/active_record/increments_customers_auth_state.rb
+++ spec/support/examples/active_record/increments_customers_auth_state.rb
@@ -2,7 +2,7 @@
 
 RSpec.shared_examples :increments_customers_auth_state do |by: 1|
   it "increments customers_auth state by #{by}" do
-    state = System::State.find(CustomersAuth.state_name)
+    state = System::State.find(CustomersAuth.state_names[0])
     expect { subject }.to change { state.reload.value }.by(by)
   end
 end
