10d0c39866566b1a80307a4a59a5384be6aefa94
artemlutsenko <artem.l@didww.com>
2025-07-24 11:48:34 +0100

    fix logging inside delayed jobs

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -15,3 +15,7 @@ end
 Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.read_ahead = 1
 Delayed::Worker.raise_signal_exceptions = true
+
+Rails.application.config.after_initialize do
+  Delayed::Worker.logger = SemanticLogger[Delayed::Worker]
+end
