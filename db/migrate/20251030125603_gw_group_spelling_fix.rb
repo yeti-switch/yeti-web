5f84f7cbe874713a52ccf10256fa8a52e7459773
Harrison Caldicott <harrison@itsfubar.com.au>
2025-12-12 20:59:20 +1100

    Upload correct fix file

--- db/migrate/20251030125603_gw_group_spelling_fix.rb
+++ db/migrate/20251030125603_gw_group_spelling_fix.rb
@@ -1,26 +1,26 @@
 class GwGroupSpellingFix < ActiveRecord::Migration[7.2]
   def up
     execute %q{
-      update class4.codecs SET name = 'Priority/Weight balancing' 
+      update class4.gateway_group_balancing_modes SET name = 'Priority/Weight balancing' 
       where name = 'Priority/Weigth balancing';
 
-      update class4.codecs SET name = 'Priority/Weight balancing. Prefer gateways from same POP' 
+      update class4.gateway_group_balancing_modes SET name = 'Priority/Weight balancing. Prefer gateways from same POP' 
       where name = 'Priority/Weigth balancing. Prefer gateways from same POP';
 
-      update class4.codecs SET name = 'Priority/Weight balancing. Exclude gateways from other POPs' 
+      update class4.gateway_group_balancing_modes SET name = 'Priority/Weight balancing. Exclude gateways from other POPs' 
       where name = 'Priority/Weigth balancing. Exclude gateways from other POPs';
     }
   end
 
   def down
     execute %q{
-      update class4.codecs SET name = 'Priority/Weigth balancing' 
+      update class4.gateway_group_balancing_modes SET name = 'Priority/Weigth balancing' 
       where name = 'Priority/Weight balancing';
 
-      update class4.codecs SET name = 'Priority/Weigth balancing. Prefer gateways from same POP' 
+      update class4.gateway_group_balancing_modes SET name = 'Priority/Weigth balancing. Prefer gateways from same POP' 
       where name = 'Priority/Weight balancing. Prefer gateways from same POP';
 
-      update class4.codecs SET name = 'Priority/Weigth balancing. Exclude gateways from other POPs' 
+      update class4.gateway_group_balancing_modes SET name = 'Priority/Weigth balancing. Exclude gateways from other POPs' 
       where name = 'Priority/Weight balancing. Exclude gateways from other POPs';
     }
   end
