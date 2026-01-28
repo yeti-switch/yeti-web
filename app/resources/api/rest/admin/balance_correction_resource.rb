28328774bf6705c90cf705ef255fe495c4b1d3a6
sdi <dmitry.s@yeti-switch.org>
2026-01-28 18:53:20 +0200

    allow account balance correction via API

---
+++ app/resources/api/rest/admin/balance_correction_resource.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+class Api::Rest::Admin::BalanceCorrectionResource < BaseResource
+  singleton
+  model_name 'Account'
+  save_form 'AdminApi::AccountBalanceCorrectionForm'
+
+  self._type = :balance_correction
+  primary_key :id
+
+  attributes :correction, :name, :balance
+
+  def correction
+    nil
+  end
+
+  def self.fetchable_fields(_context)
+    super - %i[correction]
+  end
+
+  def self.updatable_fields(_context)
+    %i[correction]
+  end
+
+  def self.creatable_fields(_context)
+    %i[correction]
+  end
+end
