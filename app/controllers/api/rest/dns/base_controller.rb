c5658d28415baf49a581bb4fea8916a3ec9ace9a
Anton Ivanov <anton.i@hey.com>
2026-02-10 15:37:09 +0200

    do not use authentication

--- app/controllers/api/rest/dns/base_controller.rb
+++ app/controllers/api/rest/dns/base_controller.rb
@@ -2,16 +2,4 @@
 
 class Api::Rest::Dns::BaseController < Api::RestController
   include JSONAPI::ActsAsResourceController
-  include AdminApiAuthorizable
-
-  before_action :set_paper_trail_whodunnit # must be after `include AdminApiAuthorizable`
-
-  def user_for_paper_trail
-    current_admin_user&.id || 'Unknown user'
-  end
-
-  def handle_exceptions(e)
-    capture_error(e) unless e.is_a?(JSONAPI::Exceptions::Error)
-    super
-  end
 end
