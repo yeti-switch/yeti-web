6906c3a8217f5cb19de54d5e3e2c5cf0a5d80153
Dmitry Sinina <dmitry.s@yeti-switch.org>
2026-03-10 21:36:22 +0200

    fixes (#1955)
    * fixes
    * bump oj

--- config/initializers/jsonapi_resources.rb
+++ config/initializers/jsonapi_resources.rb
@@ -6,6 +6,36 @@ require 'jsonapi/operation_dispatcher_patch'
 require 'jsonapi/relationship_patch'
 require 'jsonapi/request_parser_patch'
 
+# Fix Rack deprecation: :unprocessable_entity -> :unprocessable_content (HTTP 422)
+# https://github.com/cerebris/jsonapi-resources/issues/1456
+module JSONAPI
+  module Exceptions
+    class ValidationErrors < Error
+      private
+
+      def json_api_error(attr_key, message)
+        create_error_object(code: JSONAPI::VALIDATION_ERROR,
+                            status: :unprocessable_content,
+                            title: message,
+                            detail: detail(attr_key, message),
+                            source: { pointer: pointer(attr_key) },
+                            meta: metadata_for(attr_key, message))
+      end
+    end
+
+    class SaveFailed < Error
+      def errors
+        [create_error_object(code: JSONAPI::SAVE_FAILED,
+                             status: :unprocessable_content,
+                             title: I18n.translate('jsonapi-resources.exceptions.save_failed.title',
+                                                   default: 'Save failed or was cancelled'),
+                             detail: I18n.translate('jsonapi-resources.exceptions.save_failed.detail',
+                                                    default: 'Save failed or was cancelled'))]
+      end
+    end
+  end
+end
+
 JSONAPI.configure do |config|
   # can be paged, offset, none (default)
   config.default_paginator = :none
