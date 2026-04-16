a720edbc7b424286eb44db3f75f3ae6af65a8cfc
sdi <dmitry.s@yeti-switch.org>
2026-04-17 00:05:32 +0300

    fix call auth error response

--- app/controllers/api/rest/customer/v1/call_auth_controller.rb
+++ app/controllers/api/rest/customer/v1/call_auth_controller.rb
@@ -3,33 +3,40 @@
 class Api::Rest::Customer::V1::CallAuthController < Api::Rest::Customer::V1::BaseController
   include CustomerV1Authorizable
 
-  def create
-    error_options = {
-      status: '500',
-      code: '500',
-      title: 'Invalid request'
-    }
+  ConfigurationError = Class.new(JSONAPI::Exceptions::Error) do
+    def initialize(detail)
+      @detail = detail
+      super()
+    end
 
+    def errors
+      [create_error_object(
+        code: JSONAPI::INTERNAL_SERVER_ERROR,
+        status: :internal_server_error,
+        title: 'Internal Server Error',
+        detail: @detail
+      )]
+    end
+  end
+
+  def create
     if auth_context.provision_gateway_id.nil?
-      render status: 500, json: {
-        errors: [error_options.merge(detail: 'Provisioning Gateway is not found.')]
-      } and return
+      raise ConfigurationError, 'Provisioning Gateway is not found.'
     end
 
     unless auth_context.provision_gateway.incoming_auth_allow_jwt?
-      render status: 500, json: {
-        errors: [error_options.merge(detail: 'Incoming JWT is disabled for Provisioning Gateway.')]
-      } and return
+      raise ConfigurationError, 'Incoming JWT is disabled for Provisioning Gateway.'
     end
 
     private_key_path = YetiConfig&.api&.customer&.call_jwt_private_key
-    raise 'Gateway Private key not found' if private_key_path.blank? || !File.exist?(private_key_path)
+    if private_key_path.blank? || !File.exist?(private_key_path)
+      raise ConfigurationError, 'Gateway Private key not found.'
+    end
 
     private_key = OpenSSL::PKey::EC.new(File.read(private_key_path))
     lifetime = YetiConfig&.api&.customer&.call_jwt_lifetime.presence
     if private_key.blank?
-      capture_error('Private key is not found.')
-      render status: 500, json: { errors: [details: 'Internal Server Error'] } and return
+      raise ConfigurationError, 'Private key is not found.'
     end
 
     now = Time.now.to_i
@@ -41,5 +48,7 @@ class Api::Rest::Customer::V1::CallAuthController < Api::Rest::Customer::V1::Bas
 
     token = JWT.encode(payload, private_key, JwtToken::ES256)
     render json: { jwt: token }, status: 201
+  rescue StandardError => e
+    handle_exceptions(e)
   end
 end
