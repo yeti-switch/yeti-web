26de60e0159da11a588fe5897fa1d948f0a46857
sdi <dmitry.s@yeti-switch.org>
2026-05-06 17:40:38 +0300

    simplify CDRs page statistics panel UI

--- lib/resource_dsl/acts_as_cdr_stat.rb
+++ lib/resource_dsl/acts_as_cdr_stat.rb
@@ -6,7 +6,7 @@ module ResourceDSL
       section = ActiveAdmin::SidebarSection.new 'Statistic', class: 'toggle', only: :index do
         div id: 'cdr_statistic', class: 'loader'
         div id: 'cdr_statistic_placeholder', class: 'cdr-stat-grid', style: 'display: none;' do
-          div class: 'cdr-stat-cell cdr-stat-full' do
+          div class: 'cdr-stat-cell' do
             span 'Originated calls', class: 'cdr-stat-label'
             strong id: :cdr_stat_originated_calls_count, class: 'cdr-stat-value'
           end
@@ -18,7 +18,6 @@ module ResourceDSL
             span 'Rerouted', class: 'cdr-stat-label'
             strong id: :cdr_stat_rerouted_calls_count, class: 'cdr-stat-value'
           end
-          div class: 'cdr-stat-separator'
           div class: 'cdr-stat-cell' do
             span 'Duration', class: 'cdr-stat-label'
             strong id: :cdr_stat_calls_duration, class: 'cdr-stat-value'
@@ -35,17 +34,16 @@ module ResourceDSL
             span 'Term ASR', class: 'cdr-stat-label'
             strong id: :cdr_stat_termination_asr, class: 'cdr-stat-value'
           end
-          div class: 'cdr-stat-separator'
-          div class: 'cdr-stat-cell cdr-stat-full' do
+          div class: 'cdr-stat-cell' do
             span 'Profit', class: 'cdr-stat-label'
             strong id: :cdr_stat_profit, class: 'cdr-stat-value'
           end
-          div class: 'cdr-stat-cell cdr-stat-full' do
+          div class: 'cdr-stat-cell' do
             span 'Origination cost', class: 'cdr-stat-label'
             strong id: :cdr_stat_origination_cost, class: 'cdr-stat-value'
           end
           div id: :cdr_stat_origination_cost_by_currency, class: 'cdr-stat-by-currency'
-          div class: 'cdr-stat-cell cdr-stat-full' do
+          div class: 'cdr-stat-cell' do
             span 'Termination cost', class: 'cdr-stat-label'
             strong id: :cdr_stat_termination_cost, class: 'cdr-stat-value'
           end
