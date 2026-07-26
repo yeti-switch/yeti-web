da6438bb868b80b4a03c8d8d1230797a5668a33e
sdi <dmitry.s@yeti-switch.org>
2026-07-26 12:40:51 +0300

    fixes

--- app/domain/sandboxed_email_frame.rb
+++ app/domain/sandboxed_email_frame.rb
@@ -6,6 +6,6 @@ module SandboxedEmailFrame
   def render(html, style: 'width:100%;min-height:400px')
     escaped = ERB::Util.html_escape(html.to_s)
     frame_style = ERB::Util.html_escape("#{style};border:1px solid #ddd;background:#fff")
-    %(<iframe sandbox srcdoc="#{escaped}" style="#{frame_style}" title="Email body"></iframe>).html_safe
+    %(<iframe sandbox="" srcdoc="#{escaped}" style="#{frame_style}" title="Email body"></iframe>).html_safe
   end
 end
